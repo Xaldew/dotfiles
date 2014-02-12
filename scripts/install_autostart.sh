5ef24298ae96e491b566326df52189a55ee95d17
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2014-02-12 21:58:07 +0100

    Fixed autostart, now properly remaps capslock to ctrl during startup.

--- scripts/install_autostart.sh
+++ scripts/install_autostart.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 # @TODO: Actually install the autostart files.
 
+function add_autostart_files()
+{
+    dest=$1
+    for file in $DOTFILES_DIR/autostart/*; do
+	cp $file $dest
+    done
+    return 0
+}
+
 if [ -d "$XDG_CONFIG_HOME/autostart" ]; then
     echo "autostart exists."
-    for file in $DOTFILES_DIR/autostart/*; do
-	echo $file
-    done
+    add_autostart_files $XDG_CONFIG_HOME/autostart
+elif [ -d $HOME/.config/autostart ]; then
+    echo "XDG_CONFIG_HOME not set, but ~/.config/autostart exists."
+    add_autostart_files $HOME/.config/autostart
 else
-    echo "XDG_CONFIG_HOME propbably not set, or autostart does not exist."
+    echo "XDG_CONFIG_HOME not set, and ~/.config/autostart does not exist."
+    echo "Creating ~/.config/autostart..."
+    mkdir -p $HOME/.config/autostart
+    add_autostart_files $HOME/.config/autostart
 fi
