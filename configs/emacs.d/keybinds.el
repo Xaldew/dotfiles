67400eb7387763a5ba4ec54ab282908e262fcada
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2014-03-09 14:24:12 +0100

    Added some extra keybindings for M-x.

--- configs/emacs.d/keybinds.el
+++ configs/emacs.d/keybinds.el
@@ -9,3 +9,7 @@
 ;; Disable some commands.
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+
+;; Add some extra keybinding for M-x.
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
