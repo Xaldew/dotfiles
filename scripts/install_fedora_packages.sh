c375badc168750598d193da8fd952b3f75f85a0b
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2014-11-02 23:19:11 +0100

    Added initial fedora packages installation.

---
+++ scripts/install_fedora_packages.sh
@@ -0,0 +1,93 @@
+#### Install all main packages using the package manager.
+
+# Install version control systems.
+sudo yum install \
+     git \
+     bzr \
+     subversion \
+     mercurial \
+     git-bzr
+
+# Install editors and terminal mangement packages.
+sudo yum install \
+     vim \
+     ssh \
+     xclip
+
+# Install compilers and coding utilities.
+sudo yum install \
+     build-essential \
+     gcc \
+     g++ \
+     clang \
+     valgrind \
+     automake \
+     autoconf \
+     cmake \
+     doxygen \
+     ccache \
+     global \
+     silversearcher-ag
+
+# Install Python and utilities.
+sudo yum install \
+     python \
+     python3 \
+     python-pil \
+     python-scipy \
+     python-numpy \
+     pylint
+
+# Install OpenGL development utilities.
+sudo yum install \
+     freeglut3 \
+     freeglut3-dev \
+     libgl1-mesa-dev \
+     libglu1-mesa \
+     libglu1-mesa-dev \
+     libgl1-mesa-glx \
+     libx11-dev \
+     mesa-common-dev \
+     mesa-utils
+
+# Install latex and documentation utilities.
+sudo yum install \
+     texlive-full \
+     doxygen-latex
+
+
+# Install virtualization tools and cross compilers.
+sudo yum install \
+     qemu \
+     gcc-arm-linux-gnueabihf \
+     g++-arm-linux-gnueabihf \
+     binutils-arm-linux-gnueabihf
+
+# Install graphic design tools.
+sudo yum install \
+     imagemagick \
+     inkscape \
+     gimp
+
+# Install miscellaneous packages.
+sudo yum install \
+     graphviz
+
+
+# Install latest version of Tmux.
+sudo yum install libevent-dev libncurses5-dev
+tmp_dir=$(mktemp --directory)
+trap "{ cd - ; rm -rf $tmp_dir; exit 255; }" SIGINT
+
+cd $tmp_dir
+git clone git://git.code.sf.net/p/tmux/tmux-code tmux
+cd tmux
+sh autogen.sh
+./configure
+make -j
+sudo make install
+
+
+# Install the latest version of Emacs.
+sudo yum install libmagickcore-dev libmagickwand-dev \
+     libgtk-3-dev libjpeg-dev libgif-dev libtiff-dev
