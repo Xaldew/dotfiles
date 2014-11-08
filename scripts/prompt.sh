6532e1866b1a2bc37af2851423a1afdd7b1658e4
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2014-11-08 23:16:56 +0100

    Fixed some indentation errors and added some more colors.

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -18,7 +18,7 @@ parse_svn_repository_root() {
 }
 
 parse_svn_rev() {
-	svn info 2>/dev/null | sed -ne 's#^Revision: ##p' | awk '{print " SVN:"$1}'
+    svn info 2>/dev/null | sed -ne 's#^Revision: ##p' | awk '{print " SVN:"$1}'
 }
 
 # Setup standard Ubuntu prompt settings.
@@ -58,18 +58,26 @@ unset color_prompt force_color_prompt
 
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
-xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-    ;;
-*)
-    ;;
+    xterm*|rxvt*)
+	PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+	;;
+    *)
+	;;
 esac
 
-BLACK="\[\033[0;38m\]"
 RED="\[\033[0;31m\]"
 RED_BOLD="\[\033[01;31m\]"
-BLUE="\[\033[01;34m\]"
+PINK="\[\033[1;31m\]"
+YELLOW="\[\033[1;33m\]"
 GREEN="\[\033[0;32m\]"
+LT_GREEN="\[\033[1;32m\]"
+BLUE="\[\033[0;34m\]"
+WHITE="\[\033[1;37m\]"
+PURPLE="\[\033[1;35m\]"
+CYAN="\[\033[1;36m\]"
+BROWN="\[\033[0;33m\]"
+BLACK="\[\033[0;38m\]"
+COLOR_NONE="\[\033[0m\]"
 
 tt=$(tty)
 
