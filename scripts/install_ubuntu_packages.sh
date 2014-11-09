8852e8f1bcde7361cf2a19249a0e502673c8b147
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2014-11-09 13:17:44 +0100

    Added redshift to ubuntu-packages.

--- scripts/install_ubuntu_packages.sh
+++ scripts/install_ubuntu_packages.sh
@@ -7,80 +7,82 @@
 
 # Install version control systems.
 sudo apt-get --quiet=2 install \
-     git \
-     bzr \
-     subversion \
-     mercurial \
-     git-bzr
+    git \
+    bzr \
+    subversion \
+    mercurial \
+    git-bzr
 
 # Install editors and terminal mangement packages.
 sudo apt-get --quiet=2 install \
-     vim \
-     ssh \
-     xclip
+    vim \
+    ssh \
+    xclip
 
 # Install compilers and coding utilities.
 sudo apt-get --quiet=2 install \
-     build-essential \
-     gcc \
-     g++ \
-     clang \
-     valgrind \
-     automake \
-     autoconf \
-     cmake \
-     doxygen \
-     ccache \
-     global \
-     silversearcher-ag
+    build-essential \
+    gcc \
+    g++ \
+    clang \
+    valgrind \
+    automake \
+    autoconf \
+    cmake \
+    doxygen \
+    ccache \
+    global \
+    silversearcher-ag
 
 # Install Python and utilities.
 sudo apt-get --quiet=2 install \
-     python \
-     python3 \
-     python-pil \
-     python-scipy \
-     python-numpy \
-     python-pygments \
-     python3-pygments \
-     pylint
+    python \
+    python3 \
+    python-pil \
+    python-scipy \
+    python-numpy \
+    python-pygments \
+    python3-pygments \
+    pylint
 
 # Install OpenGL development utilities.
 sudo apt-get --quiet=2 install \
-     freeglut3 \
-     freeglut3-dev \
-     libgl1-mesa-dev \
-     libglu1-mesa \
-     libglu1-mesa-dev \
-     libgl1-mesa-glx \
-     libx11-dev \
-     mesa-common-dev \
-     mesa-utils
+    freeglut3 \
+    freeglut3-dev \
+    libgl1-mesa-dev \
+    libglu1-mesa \
+    libglu1-mesa-dev \
+    libgl1-mesa-glx \
+    libx11-dev \
+    mesa-common-dev \
+    mesa-utils
 
 # Install latex and documentation utilities.
 sudo apt-get --quiet=2 install \
-     texlive-full \
-     doxygen-latex
+    texlive-full \
+    doxygen-latex
 
 
 # Install virtualization tools and cross compilers.
 sudo apt-get --quiet=2 install \
-     qemu \
-     gcc-arm-linux-gnueabihf \
-     g++-arm-linux-gnueabihf \
-     binutils-arm-linux-gnueabihf
+    qemu \
+    gcc-arm-linux-gnueabihf \
+    g++-arm-linux-gnueabihf \
+    binutils-arm-linux-gnueabihf
 
 # Install graphic design tools.
 sudo apt-get --quiet=2 install \
-     imagemagick \
-     inkscape \
-     gimp
+    imagemagick \
+    inkscape \
+    gimp
 
 # Install miscellaneous packages.
 sudo apt-get --quiet=2 install \
-     graphviz \
-     remmina \
-     remmina-plugins-nx
+    graphviz \
+    remmina \
+    remmina-plugins-nx \
+    redshift \
+    redshift-gtk
 
 
 # Install latest version of Tmux.
@@ -99,7 +101,7 @@ sudo make install
 
 # Install the latest version of Emacs.
 sudo apt-get --quiet=2 install libmagickcore-dev libmagickwand-dev \
-     libgtk-3-dev libjpeg-dev libgif-dev libtiff-dev
+    libgtk-3-dev libjpeg-dev libgif-dev libtiff-dev
 cd $tmp_dir
 git clone git://git.savannah.gnu.org/emacs.git
 sh autogen.sh
