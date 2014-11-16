269c10cec53ee96464274d7fe101aa02809a6f90
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2014-11-16 16:13:45 +0100

    Fixed some file associations for make and scons.

--- configs/emacs.d/file-assoc.el
+++ configs/emacs.d/file-assoc.el
@@ -10,16 +10,16 @@
 (autoload 'glsl-mode "glsl-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
 (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
-(add-to-list 'auto-mode-alist '("\\.vs\\'" . glsl-mode))
-(add-to-list 'auto-mode-alist '("\\.fs\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.vs\\'"   . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.fs\\'"   . glsl-mode))
 
 ;; Add makefile modes.
-(add-to-list 'auto-mode-alist '("\\.mak\\'" . makefile-mode))
-(add-to-list 'auto-mode-alist '("\\.build\\'" . makefile-mode))
+(add-to-list 'auto-mode-alist '("\\.mak\\'"   . makefile-gmake-mode))
+(add-to-list 'auto-mode-alist '("\\.build\\'" . makefile-gmake-mode))
 
 ;; Set Scons files to python mode.
-(add-to-list 'auto-mode-alist '("[Ss][Cc]onstruct" . python-mode))
-(add-to-list 'auto-mode-alist '("[Ss][Cc]onscript" . python-mode))
+(add-to-list 'auto-mode-alist '("[Ss][Cc]onstruct\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("[Ss][Cc]onscript\\'" . python-mode))
 
 ;; Associate PBRT files with pbrt-model.
 (add-to-list 'auto-mode-alist '("\\.pbrt\\'" . pbrt-mode))
