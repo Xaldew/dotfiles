949ebdb9bcdcc45a27c1420c9ef43c8572474f09
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2014-11-19 15:16:22 +0100

    Added a simple script that can install llvm.
    This script accepts a string with the tag for llvm and then installs
    all components of llvm for that tag.

---
+++ scripts/install_llvm.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# A small script to checkout everything related to llvm and clang for a
+# given release.
+
+release=$1
+svn checkout http://llvm.org/svn/llvm-project/llvm/$release llvm
+
+if [ $? -eq 0 ]; then
+    (cd llvm/tools &&
+	    svn checkout http://llvm.org/svn/llvm-project/cfe/$release clang)
+    (cd llvm/projects &&
+	    svn checkout http://llvm.org/svn/llvm-project/compiler-rt/$release \
+		compiler-rt)
+else
+    echo "Error: Failed to download LLVM."
+fi
