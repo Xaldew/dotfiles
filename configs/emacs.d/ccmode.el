76e764f57ee8a5d82414701882ff54408351b5eb
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2014-12-03 22:32:44 +0100

    Fixed bug in CC-mode font-highlighting.
    Apparently, cc-init-hook is only run once, so font-highlightings has to
    be placed in c-mode-common-hook instead.
    
    Also added highlighter for true/false since that for some reasons
    wasn't on by default anymore.

--- configs/emacs.d/ccmode.el
+++ configs/emacs.d/ccmode.el
@@ -2,14 +2,8 @@
 
 ;; Activate Javadoc comment highlighting for C and C++.
 (defun my-cc-init-hook ()
-  "Initialization hook for CC-mode runs before any other hooks."
-  (setq c-doc-comment-style
-	'((java-mode . javadoc)
-	  (pike-mode . autodoc)
-	  (c-mode    . javadoc)
-	  (c++-mode  . javadoc)))
-  (set-face-foreground 'font-lock-doc-face
-		       (face-foreground font-lock-comment-face)))
+  "Initialization hook for CC-mode runs before any other hooks,
+ but only once per Emacs session.")
 (add-hook 'c-initialization-hook 'my-cc-init-hook)
 
 (defvar font-lock-format-specifier-face
@@ -23,9 +17,19 @@
 
 ;; Customizations for all modes in CC Mode.
 (defun my-cc-mode-common-hook ()
+  "Setup common utilities for all C-like modes."
+  (setq c-doc-comment-style
+	'((java-mode . javadoc)
+	  (pike-mode . autodoc)
+	  (c-mode    . javadoc)
+	  (c++-mode  . javadoc)))
+  (set-face-foreground 'font-lock-doc-face
+		       (face-foreground font-lock-comment-face))
   (font-lock-add-keywords
    nil
-   '(("\\<\\(FIXME\\|TODO\\):" 1 font-lock-warning-face prepend)
+   '(("\\<\\(FIXME\\|TODO\\):?" 1 font-lock-warning-face prepend)
+     ;; Add extra constants for true/false.
+     ("\\<\\(true\\|false\\)" . font-lock-constant-face)
      ;; Add a printf() modifier highlighter.
      ("[^%]\\(%\\([[:digit:]]+\\$\\)?[-+' #0*]*\\([[:digit:]]*\\|\\*\\|\\*[[:digit:]]+\\$\\)\\(\\.\\([[:digit:]]*\\|\\*\\|\\*[[:digit:]]+\\$\\)\\)?\\([hlLjzt]\\|ll\\|hh\\)?\\([aAbdiuoxXDOUfFeEgGcCsSpn]\\|\\[\\^?.[^]]*\\]\\)\\)"
       1 font-lock-format-specifier-face prepend))) )
