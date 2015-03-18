f901e835845da5bb3a67e3d6cad5b7abb4535550
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-03-18 16:36:34 +0100

    Bound M-* to pop-tag-mark as in Emacs pre-25.1.

--- configs/emacs.d/keybinds.el
+++ configs/emacs.d/keybinds.el
@@ -14,5 +14,8 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
-;; Disable some key-bindings.
+;; Disable the character transpose (C-t) keybinding, which is used by Tmux.
 (global-unset-key "\C-t")
+
+;; Enable M-* to pop-tag-mark. This is the default in Emacs pre-25.1.
+(global-set-key "\M-*" 'pop-tag-mark)
