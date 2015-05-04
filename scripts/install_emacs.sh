ca5843ea36a791137e3264b8a1dcbf515dd798aa
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-05-04 23:28:37 +0200

    Updating install_emacs.sh script.
    The script now downloads, compiles and install emacs 24 and 25.

--- scripts/install_emacs.sh
+++ scripts/install_emacs.sh
@@ -16,12 +16,33 @@ cd $HOME/git/installs
 
 if [ -d $HOME/git/installs/emacs ]; then
     cd $HOME/git/installs/emacs
+    make distclean
+    git clean --force
+    git checkout -B master remotes/origin/master
+    git pull
+    git checkout -B emacs-24 remotes/origin/emacs-24
     git pull
 else
     git clone git://git.savannah.gnu.org/emacs.git emacs
 fi
 
+# Compile and install Emacs-24.
 cd $HOME/git/installs/emacs
+git checkout -B emacs-24 origin/emacs-24
+sh autogen.sh
+./configure --prefix=$HOME/.local \
+	    --mandir=$HOME/.local/share/man \
+	    --enable-link-time-optimization \
+	    --with-imagemagick \
+	    --with-x-toolkit=lucid
+make -j
+make install
+
+
+# Compile and install the latest Emacs.
+make distclean
+git clean --force
+git checkout -B master origin/master
 sh autogen.sh
 ./configure --prefix=$HOME/.local \
 	    --mandir=$HOME/.local/share/man \
