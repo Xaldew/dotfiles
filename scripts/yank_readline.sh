d17680c74b6413522e11753293f8dd131bb57528
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-05-10 11:18:49 +0200

    Adding a script to integrate xclip and bash.
    This script uses xclip, bash and GNU readline to integrate the the X
    Clipboards with readline inside bash.
    
    - Use M-k to kill a line to clipboard.
    - Use M-y to yank a line from the clipboard. (This is paste, vim users.)
    - Use M-u to discard a character.

---
+++ scripts/yank_readline.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Utilities for integrating readline and the X clipboards.
+
+# Verify that xclip is available, and that bash is at least version 4.
+if command -v xclip > /dev/null 2>&1 && [ $BASH_VERSINFO -ge 4 ]; then
+
+    function _xdiscard()
+    {
+	echo -n "${READLINE_LINE:0:$READLINE_POINT}" | \
+	    xclip -selection clipboard
+	READLINE_LINE="${READLINE_LINE:$READLINE_POINT}"
+	READLINE_POINT=0
+    }
+    function _xkill()
+    {
+	echo -n "${READLINE_LINE:$READLINE_POINT}" | \
+	    xclip -selection clipboard
+	READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}"
+    }
+    function _xyank()
+    {
+	clip=$(xclip -selection clipboard -out)
+	count=$(echo -n "$clip" | wc -c)
+	READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}"
+	READLINE_LINE+="${clip}${READLINE_LINE:$READLINE_POINT}"
+	READLINE_POINT=$(($READLINE_POINT + $count))
+    }
+
+    # Add keybindings M-k/M-k/M-u to discard/kill/yank to/from the clipboard
+    # through readline.
+    bind -m emacs -x '"\eu": _xdiscard'
+    bind -m emacs -x '"\ek": _xkill'
+    bind -m emacs -x '"\ey": _xyank'
+fi
