87997a87e81afadac83050ceaa9e364ab75bb1c9
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-05-11 16:43:09 +0200

    Added 256-color terminals to colorbar script.

--- scripts/colorbars.py
+++ scripts/colorbars.py
@@ -1,41 +1,6 @@
 #!/usr/bin/env python
 # Display the terminal color bars along the regular true-color equivalent.
 
-
-"""
-Reference for colormaps included with Matplotlib.
-
-This reference example shows all colormaps included with Matplotlib. Note that
-any colormap listed here can be reversed by appending "_r" (e.g., "pink_r").
-These colormaps are divided into the following categories:
-
-Sequential:
-    These colormaps are approximately monochromatic colormaps varying smoothly
-    between two color tones---usually from low saturation (e.g. white) to high
-    saturation (e.g. a bright blue). Sequential colormaps are ideal for
-    representing most scientific data since they show a clear progression from
-    low-to-high values.
-
-Diverging:
-    These colormaps have a median value (usually light in color) and vary
-    smoothly to two different color tones at high and low values. Diverging
-    colormaps are ideal when your data has a median value that is significant
-    (e.g.  0, such that positive and negative values are represented by
-    different colors of the colormap).
-
-Qualitative:
-    These colormaps vary rapidly in color. Qualitative colormaps are useful for
-    choosing a set of discrete colors. For example::
-
-        color_list = plt.cm.Set3(np.linspace(0, 1, 12))
-
-    gives a list of RGB colors that are good for plotting a series of lines on
-    a dark background.
-
-Miscellaneous:
-    Colormaps that don't fit into the categories above.
-
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
@@ -55,13 +20,27 @@ def create_colormaps():
     plt.register_cmap(cmap=colors.ListedColormap(lgray,  name="tc_gray"))
 
     # Generate 256-color terminal colormaps.
+    term256_hex = ["00","5f","87","af","d7","ff"]
+    term256_hex_gray = ["00", "08", "12", "1c", "26", "30", "3A", "44", "4e",
+                        "58", "62", "6c", "76", "80", "8a", "94", "9e", "a8",
+                        "b2", "bc", "c6", "d0", "da", "e4", "ee"]
+    term256_r = ["#{}0000".format(h) for h in term256_hex]
+    term256_g = ["#00{}00".format(h) for h in term256_hex]
+    term256_b = ["#0000{}".format(h) for h in term256_hex]
+    term256_gray = ["#{0:s}{0:s}{0:s}".format(h) for h in term256_hex_gray]
+
+    plt.register_cmap(cmap=colors.ListedColormap(term256_r,    name="term256_red"))
+    plt.register_cmap(cmap=colors.ListedColormap(term256_g,    name="term256_green"))
+    plt.register_cmap(cmap=colors.ListedColormap(term256_b,    name="term256_blue"))
+    plt.register_cmap(cmap=colors.ListedColormap(term256_gray, name="term256_gray"))
 
     # Generate 88-color terminal colormaps.
-    term88_r = ["#000000", "#8b0000", "#cd0000", "#ff0000"]
-    term88_g = ["#000000", "#008b00", "#00cd00", "#00ff00"]
-    term88_b = ["#000000", "#00008b", "#0000cd", "#0000ff"]
-    term88_gray = ["#{0:s}{0:s}{0:s}".format(h) for h in
-                   ["2e", "5c", "73", "8b", "a2", "b9", "d0", "e7"]]
+    term88_hex = ["00", "8b", "cd", "ff"]
+    term88_hex_gray = ["00", "2e", "5c", "73", "8b", "a2", "b9", "d0", "e7"]
+    term88_r = ["#{}0000".format(h) for h in term88_hex]
+    term88_g = ["#00{}00".format(h) for h in term88_hex]
+    term88_b = ["#0000{}".format(h) for h in term88_hex]
+    term88_gray = ["#{0:s}{0:s}{0:s}".format(h) for h in term88_hex_gray]
 
     plt.register_cmap(cmap=colors.ListedColormap(term88_r,    name="term88_red"))
     plt.register_cmap(cmap=colors.ListedColormap(term88_g,    name="term88_green"))
@@ -69,11 +48,10 @@ def create_colormaps():
     plt.register_cmap(cmap=colors.ListedColormap(term88_gray, name="term88_gray"))
 
     return ["tc_red", "tc_green", "tc_blue", "tc_gray",
-            "term88_red", "term88_green", "term88_blue", "term88_gray"]
+            "term88_red", "term88_green", "term88_blue", "term88_gray",
+            "term256_red", "term256_green", "term256_blue", "term256_gray"]
 
-cmaps = [('Sequential',     create_colormaps())]
-
-
+cmaps = [('Terminal',     create_colormaps())]
 
 
 nrows = max(len(cmap_list) for cmap_category, cmap_list in cmaps)
