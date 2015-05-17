3bbe25b36e33ab25e9be8e2d3de2049b0cde5d1c
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-05-17 13:09:27 +0200

    Exuberant Ctags uses updated environment paths.

--- scripts/install_exuberant_ctags.sh
+++ scripts/install_exuberant_ctags.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
-# Install latest Exuberant ctags.
+# Install latest Exuberant ctags and rename it appropriately.
 
-cd $HOME/git/installs/
-
-if [ -d $HOME/git/installs/exuberant-ctags ]; then
-    cd $HOME/git/installs/exuberant-ctags
+cd $objects_dir
+if [ -d $objects_dir/exuberant-ctags ]; then
+    cd $objects_dir/exuberant-ctags
     make distclean
     git reset --hard
     git clean --force
@@ -14,14 +13,14 @@ else
 	svn://svn.code.sf.net/p/ctags/code/trunk exuberant-ctags
 fi
 
-cd $HOME/git/installs/exuberant-ctags
+cd $objects_dir/exuberant-ctags
 
 sed --in-place 's|^\(CTAGS_PROG\s*=\s*\)ctags|\1 ex-ctags|g' Makefile.in
 sed --in-place 's|^\(ETAGS_PROG\s*=\s*\)etags|\1 ex-etags|g' Makefile.in
 autoreconf
 ./configure \
-    --prefix=$HOME/.local \
-    --mandir=$HOME/.local/share/man \
+    --prefix=$local_prefix_dir \
+    --mandir=$local_prefix_dir/share/man \
     --disable-etags
 make -j4
 make install
