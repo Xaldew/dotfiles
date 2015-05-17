2aa1a62a92e735dc1ff20e464627badab43771f6
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-05-17 23:03:38 +0200

    Removed redundant prompt.sh setup.
    A number of things Ubuntu uses for their default prompt has been removed
    since it wasn't being used.

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -156,41 +156,6 @@ cmd_status()
     fi
 }
 
-# Setup standard Ubuntu prompt settings.
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
-
-# set a fancy prompt (non-color, unless we know we "want" color)
-case "$TERM" in
-    xterm-color) color_prompt=yes;;
-esac
-
-# Uncomment for a colored prompt, if the terminal has the capability; turned
-# off by default to not distract the user: the focus in a terminal window
-# should be on the output of the commands, not on the prompt
-force_color_prompt=yes
-if [ -n "$force_color_prompt" ]; then
-    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-        # We have color support; assume it's compliant with Ecma-48
-        # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-        # a case would tend to support setf rather than setaf.)
-        color_prompt=yes
-    else
-        color_prompt=
-    fi
-fi
-
-# If this is an xterm set the title to user@host:dir
-case "$TERM" in
-    xterm*|rxvt*)
-	PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-	;;
-    *)
-	;;
-esac
-
 # Set the prompt.
 set_prompt()
 {
@@ -199,8 +164,6 @@ set_prompt()
     PS1+=$(scm_status)
     PS1+="]$(cmd_status ${last_status}) "
 }
-
-tt=$(tty)
 
 if [[ $(tty) =~ .*tty^s.* ]] || [[ "$TERM" =~ ^linux$ ]]; then
     export PROMPT_COMMAND='set_prompt'
