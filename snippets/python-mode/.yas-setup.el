56f2ea65ebbe40346ab2317c9476a7aadd7cfdbe
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-06-10 17:03:22 +0200

    Minor update to Yasnippet Python reST docstring.

--- snippets/python-mode/.yas-setup.el
+++ snippets/python-mode/.yas-setup.el
@@ -2,27 +2,30 @@
   "Return a reST docstring format for the python arguments in yas-text."
   (let* ((indent (concat "\n" (make-string (current-column) 32)))
          (args (python-split-args yas-text))
-         (max-len (if args
-		      (apply 'max (mapcar
-				   (lambda (x) (length (nth 0 x))) args)) 0))
          (formatted-args
 	  (mapconcat
 	   (lambda (x)
 	     (concat ":param: " (nth 0 x) ":"
 		     (if (nth 1 x) (concat "\(default " (nth 1 x) "\)"))))
 	   args
-	   indent))
-	 (formatted-types
-	  (mapconcat
-	   (lambda (x)
-	     (concat ":type: " (nth 0 x) ":"))
-	   args
 	   indent)))
     (unless (string= formatted-args "")
       (concat
        indent
        (mapconcat 'identity
-		  (list ".. Keyword Arguments:" formatted-args ""
-			".. Types:" formatted-types)
+		  (list ".. Keyword Arguments:" formatted-args)
 		  indent)
+       indent))))
+
+
+(defun python-arg-types-to-reST-docstring ()
+  "Return a reST docstring format for the python argument types in yas-text."
+  (let* ((indent (concat "\n" (make-string (current-column) 32)))
+         (args (python-split-args yas-text))
+	 (formatted-types
+	  (mapconcat (lambda (x) (concat ":type: " (nth 0 x) ":"))
+		     args indent)))
+    (unless (string= formatted-types "")
+      (concat
+       (mapconcat 'identity (list  ".. Types:" formatted-types) indent)
        indent))))
