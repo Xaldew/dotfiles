e951a1a56c06a3696c6cce28cef8ca1ef22c2a5d
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-06-17 17:04:45 +0200

    Some more work on the stacked yasnippets.

--- snippets/python-mode/.yas-setup.el
+++ snippets/python-mode/.yas-setup.el
@@ -1,14 +1,17 @@
-(defun python-args-to-reST-docstring ()
+(defun python-args-to-reST-docstring (text &optional make-fields)
   "Return a reST docstring format for the python arguments in yas-text."
   (let* ((indent (concat "\n" (make-string (current-column) 32)))
-         (args (python-split-args yas-text))
+         (args (python-split-args text))
+	 (nr 0)
          (formatted-args
 	  (mapconcat
 	   (lambda (x)
 	     (concat ":param: " (nth 0 x) ":"
-		     (if (nth 1 x) (concat "\(default " (nth 1 x) "\)"))))
+		     (if make-fields (format " ${%d:arg%d} " (incf nr) nr))
+		     (if (nth 1 x) (concat " \(default " (nth 1 x) "\)"))))
 	   args
 	   indent)))
+    (setq python-reST-snippet text)
     (unless (string= formatted-args "")
       (concat
        indent
@@ -29,3 +32,16 @@
       (concat
        (mapconcat 'identity (list  ".. Types:" formatted-types) indent)
        indent))))
+
+
+(defvar python-reST-snippet nil "Variable to store snippet content in.")
+
+(defun yas-stacked-snippet (fun snippet-sym)
+  "Expand a snippet after passing it through the given function."
+  (let ((snippet (symbol-value snippet-sym)))
+    (print yas-text)
+    (print fun)
+    (print snippet-sym)
+    (print snippet)
+    (print (concat (funcall fun snippet t) "$0"))
+    (yas-expand-snippet (concat (funcall fun snippet t) "$0"))))
