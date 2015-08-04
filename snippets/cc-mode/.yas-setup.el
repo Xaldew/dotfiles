73e14bf6f80abefe6a0a443e4f1a1cf3c162a085
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-08-04 16:51:47 +0200

    Moved fun_declaration snippet to cc-mode snippets.
    This snippet is applicable to both C/C++, thus it was "moved up" the
    mode hierarchy.
    
    Also, there was a bug in the snippet when expanded inside C++ classes.
    This is fixed in this commit.
    
    Debug-prints are also removed from the doxy snippet.

--- snippets/cc-mode/.yas-setup.el
+++ snippets/cc-mode/.yas-setup.el
@@ -3,6 +3,56 @@
 	  (lambda () (setq-local yas-start-point (point))))
 
 
+(defun c++-split-args (fn-name arg-string)
+  "Split a C++ argument string into a list of names."
+  (if (and fn-name (bound-and-true-p semantic-mode))
+      (let* ((tag (or (semantic-current-tag)
+		      (semantic-analyze-find-tag fn-name 'function)))
+	     (class   (if tag (semantic-tag-class tag) 'other))
+	     (attrs   (if tag (semantic-tag-attributes tag) '()))
+	     (members (if attrs (plist-get attrs :members) '()))
+	     (found (not members))
+	     (args nil))
+	(while (and (not found) members) ; Find the member inside a class.
+	  (setq tag (pop members))
+	  (when (string= (semantic-tag-name tag) fn-name)
+	    (setq found t)))
+	(setq attrs (semantic-tag-attributes tag))
+	(setq args (delete "" (mapcar 'car (plist-get attrs :arguments)))))
+    (mapcar (lambda (x)
+	      (nth 1 (split-string x)))
+	    (split-string arg-string "[[:blank:]]*,[[:blank:]]*" t))))
+
+
+(defun c++-doxy-docstring (text fn-name return-value &optional make-fields)
+  "Return docstring format for the C++ arguments in `text'."
+  (let* ((indent (concat "\n" (make-string (current-column) 32)))
+         (args (c++-split-args fn-name text))
+         (max-len (if args
+		      (apply 'max (mapcar (lambda (x) (length x)) args))
+		    0))
+	 (nr 0)
+         (formatted-args
+	  (mapconcat
+	   (lambda (x) (concat
+			"@param "
+			x (make-string (- max-len (length x)) ? )
+			(if make-fields (format " ${%d:arg%d}" (incf nr) nr))))
+	   args
+	   indent)))
+    (concat
+     (unless (string= formatted-args "")
+       (concat
+	indent (mapconcat 'identity (list formatted-args) indent)))
+     (if (and (> (length args) 3)
+	      (stringp return-value)
+	      (string-match-p "^.*void" return-value))
+	 indent)
+     (unless (and (stringp return-value)
+		  (string-match-p "^.*void" return-value))
+       (format "%s@return ${%d:Returns...}" indent (1+ nr))))))
+
+
 (defun yas-cc-mode-brace (brace syntax)
   "Determine the open brace and newline locations inside a snippet.
 
