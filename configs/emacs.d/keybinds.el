3784d97d667d0820614554a375ed836f09f3392e
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-08-07 15:11:09 +0200

    Changed Emacs keys to use the kbd-function.

--- configs/emacs.d/keybinds.el
+++ configs/emacs.d/keybinds.el
@@ -1,22 +1,22 @@
-;; Set some global keybindings.
+;; Set some global key bindings.
 (global-set-key [f5]  'compile)
 (global-set-key [f6]  'vc-dir)
 (global-set-key [f7]  'gdb)
 (global-set-key [f8]  'shell)
 (global-set-key [f9]  'toggle-truncate-lines)
-(global-set-key "\C-h" 'backward-delete-char)
-(global-set-key "\M-h" 'backward-kill-word)
+(global-set-key (kbd "C-h") 'backward-delete-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
 
 ;; Disable some commands.
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
-;; Add some extra keybinding for M-x.
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
+;; Add some extra key binding for M-x.
+(global-set-key (kbd "C-x C-m") 'execute-extended-command)
+(global-set-key (kbd "C-c C-m") 'execute-extended-command)
 
 ;; Disable the character transpose (C-t) keybinding, which is used by Tmux.
-(global-unset-key "\C-t")
+(global-unset-key (kbd "C-t"))
 
 ;; Enable M-* to pop-tag-mark. This is the default in Emacs pre-25.1.
-(global-set-key "\M-*" 'pop-tag-mark)
+(global-set-key (kbd "M-*") 'pop-tag-mark)
