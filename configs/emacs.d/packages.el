d2231e016b9465f6db04caabe6b416c4361931b0
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-08-22 20:24:54 +0200

    Added paredit package and renamed package list.

--- configs/emacs.d/packages.el
+++ configs/emacs.d/packages.el
@@ -11,85 +11,88 @@
 
 
 ;; Create a list of packages we want to be installed.
-(setq package-list '(ecb
-		     highlight-indentation
-		     google-c-style
-		     hide-comnt
-		     company
-		     company-math
-		     company-irony
-		     company-anaconda
-		     company-c-headers
-		     company-quickhelp
-		     rainbow-mode
-		     graphviz-dot-mode
-		     glsl-mode
-		     cmake-mode
-		     xclip
-		     yasnippet
-		     srefactor
-		     expand-region
-		     gitignore-mode
-		     gitconfig-mode
-		     ssh-config-mode
-		     evil
-		     elpy
-		     anaconda-mode
-		     web-mode
-		     dart-mode
-		     magit
-		     auto-complete
-		     ac-etags
-		     ac-anaconda
-		     auto-complete-clang
-		     flycheck
-		     flycheck-irony
-		     flycheck-pos-tip
-		     ac-irony
-		     irony
-		     auto-complete-c-headers
-		     c-eldoc
-		     css-eldoc
-		     irony-eldoc
-		     ggtags
-		     csv-mode
-		     cuda-mode
-		     coffee-mode
-		     projectile
-		     clojure-mode
-		     clojure-mode-extra-font-locking
-		     cider
-		     rainbow-delimiters
-		     zenburn-theme
-		     sublime-themes
-		     markdown-mode
-		     dropdown-list
-		     powershell
-		     ace-window
-		     ace-jump-mode
-		     form-feed
-		     auctex
-		     auctex-latexmk
-		     auto-complete-auctex
-		     company-auctex
-		     undo-tree))
+(setq my-package-list '(ecb
+			highlight-indentation
+			google-c-style
+			hide-comnt
+			paredit
+			company
+			company-math
+			company-irony
+			company-anaconda
+			company-c-headers
+			company-quickhelp
+			rainbow-mode
+			graphviz-dot-mode
+			glsl-mode
+			cmake-mode
+			xclip
+			yasnippet
+			srefactor
+			expand-region
+			gitignore-mode
+			gitconfig-mode
+			ssh-config-mode
+			evil
+			elpy
+			anaconda-mode
+			web-mode
+			dart-mode
+			magit
+			auto-complete
+			ac-etags
+			ac-anaconda
+			auto-complete-clang
+			flycheck
+			flycheck-irony
+			flycheck-pos-tip
+			ac-irony
+			irony
+			auto-complete-c-headers
+			c-eldoc
+			css-eldoc
+			irony-eldoc
+			ggtags
+			csv-mode
+			cuda-mode
+			coffee-mode
+			projectile
+			clojure-mode
+			clojure-mode-extra-font-locking
+			cider
+			rainbow-delimiters
+			zenburn-theme
+			sublime-themes
+			markdown-mode
+			dropdown-list
+			powershell
+			ace-window
+			ace-jump-mode
+			form-feed
+			auctex
+			auctex-latexmk
+			auto-complete-auctex
+			company-auctex
+			undo-tree))
 
 
 ;; Create an alist of where we should retrieve certain packages.
 (setq package-pinned-packages
       '((coffee-mode . "melpa")
-	(form-feed . "melpa")))
+	(form-feed   . "melpa")
+	(paredit     . "melpa")))
 
 
 (or (file-exists-p package-user-dir)
     (package-refresh-contents))
 
 
-(dolist (package package-list)
+(dolist (package my-package-list)
   (unless (package-installed-p package)
     (condition-case nil
 	(package-install package)
       (error (message "[ERROR]: Failed to install package: %s." package)))))
+
 
 ;; Remove no longer needed packages.
 (when (fboundp 'package-autoremove)
