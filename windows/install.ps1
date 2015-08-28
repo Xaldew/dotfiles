c55d0330377d7fb5d80848b631a5eb0f47abb71c
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-08-28 16:21:07 +0200

    De-DOS-ed the install.ps1 script.
    Also updated download directory and various other tweaks.

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,51 +1,46 @@
-$source = 'C:\source'
-If (!(Test-Path -Path $source -PathType Container))
-{
-    New-Item -Path $source -ItemType Directory | Out-Null
-}
-
 $packages = @(
     @{
 	title='7zip Extractor';
 	url='http://www.7-zip.org/a/7z1506-x64.exe';
-	Arguments=' /qn';
-	Destination=$source
+	Arguments='';
     },
     @{
 	title='Git';
 	url='https://github.com/git-for-windows/git/releases/download/v2.5.0.windows.1/Git-2.5.0-64-bit.exe';
-	Arguments=' /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-';
-	Destination=$source
+	Arguments='';
     },
     @{
 	title='Emacs 24.5';
 	url='http://ftp.gnu.org/gnu/emacs/windows/emacs-24.5-bin-i686-mingw32.zip';
 	Arguments='';
-	Destination=$source
     },
     @{
 	title='Notepad++ 6.8.2';
 	url='https://notepad-plus-plus.org/repository/6.x/6.8.2/npp.6.8.2.Installer.exe';
-	Arguments=' /Q /S';
-	Destination=$source
+	Arguments='';
     },
     @{
 	title='AutoHotkey';
 	url='http://ahkscript.org/download/ahk-install.exe'
 	Arguments='';
-	Destination=$source
-    }
+    },
 )
 
+$downloadDir = [io.path]::combine([Environment]::GetFolderPath("MyDocuments"), "installs")
+If (!(Test-Path -Path $downloadDir -PathType Container))
+{
+    New-Item -Path $downloadDir -ItemType Directory | Out-Null
+}
+
+$webClient = New-Object System.Net.WebClient
 foreach ($pkg in $packages)
 {
     $pkgName = $pkg.title
     $fileName = [io.path]::GetFileName($pkg.url)
-    $dstPath = [io.path]::combine($pkg.Destination, $fileName)
+    $dstPath = [io.path]::combine($downloadDir, $fileName)
     If (!(Test-Path -Path $dstPath -PathType Leaf))
     {
 	Write-Host "Downloading $pkgName"
-	$webClient = New-Object System.Net.WebClient
 	$webClient.DownloadFile($pkg.url, $dstPath)
     }
 }
@@ -56,8 +51,8 @@ foreach ($pkg in $packages)
 {
     $pkgName = $pkg.title
     $fileName = [io.path]::GetFileName($pkg.url)
-    $dstPath = [io.path]::combine($pkg.Destination, $fileName)
+    $dstPath = [io.path]::combine($downloadDir, $fileName) | Get-Item
     $Arguments = $pkg.Arguments
-    Write-Output "Installing $pkgName"
-    #    Invoke-Expression -Command "$dstPath $Arguments"
+    Write-Output "Installing $pkgName..."
+    #Invoke-Expression -Command "$dstPath $Arguments"
 }
