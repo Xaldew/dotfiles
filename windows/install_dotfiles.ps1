458eb8f0f736ed56cbd12814c42c36581383cba1
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-08-28 16:25:38 +0200

    Added script that installs dotfiles for Windows.

--- windows/install_dotfiles.ps1
+++ windows/install_dotfiles.ps1
@@ -6,9 +6,11 @@ Write-Host "AppData location: $Env:APPDATA"
 Write-Host "Script location: $scriptPath"
 Write-Host "Dotfiles location: $dotfilesDir"
 
+
 # Copy all Bash configuration.
 Copy-Item -Path $dotfilesDir/configs/bashrc -Destination $HOME/.bashrc -Force
 Copy-Item -Path $dotfilesDir/configs/bash_aliases -Destination $HOME/.bash_aliases -Force
+
 
 # Copy all SSH configuration.
 If (!(Test-Path -Path $HOME/.ssh -PathType Container))
@@ -17,11 +19,16 @@ If (!(Test-Path -Path $HOME/.ssh -PathType Container))
 }
 Copy-Item -Path $dotfilesDir/configs/ssh_config -Destination $HOME/.ssh/config -Force
 
+
 # Copy all Git configuration.
-Copy-Item -Path $dotfilesDir/configs/gitconfig -Destination $HOME/.gitconfig -Force
+Copy-Item -Path $dotfilesDir/windows/gitconfig -Destination $HOME/.gitconfig -Force
 Copy-Item -Path $dotfilesDir/configs/gitignore -Destination $HOME/.gitignore -Force
 
 
 # Copy all Emacs configuration.
-# Note that it is placed in APPDATA instead of HOME since that's where Emacs
-# look for configuration files by default.
+# Note that it is placed in APPDATA instead of HOME since that is where Emacs
+# looks for configuration files by default.
+Copy-Item $dotfilesDir/configs/emacs.d/ -Destination $Env:APPDATA/.emacs.d/ -Force -Recurse
+
+
+# Copy all AutoHotkey configuration.
