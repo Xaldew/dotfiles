8ce8df03be66564d3ee45765f348a2e84c82770e
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-09-12 12:53:01 +0200

    Removed SSH config from windows install.

--- windows/install_dotfiles.ps1
+++ windows/install_dotfiles.ps1
@@ -12,14 +12,6 @@ Copy-Item -Path $dotfilesDir/configs/bashrc -Destination $HOME/.bashrc -Force
 Copy-Item -Path $dotfilesDir/configs/bash_aliases -Destination $HOME/.bash_aliases -Force
 
 
-# Copy all SSH configuration.
-If (!(Test-Path -Path $HOME/.ssh -PathType Container))
-{
-    New-Item -Path $HOME/.ssh -ItemType Directory | Out-Null
-}
-Copy-Item -Path $dotfilesDir/configs/ssh_config -Destination $HOME/.ssh/config -Force
-
-
 # Copy all Git configuration.
 Copy-Item -Path $dotfilesDir/windows/gitconfig -Destination $HOME/.gitconfig -Force
 Copy-Item -Path $dotfilesDir/configs/gitignore -Destination $HOME/.gitignore -Force
