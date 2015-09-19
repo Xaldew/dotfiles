bd324c1bf44ed26ab3ddfea6bf64d1872c5f2777
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-09-19 15:45:29 +0200

    Emacs: `packages.el` greatly simplified.
    This module now *only* installs use-package if it is not already
    installed.
    
    It may be expanded as an install scripts at some point however to reduce
    the number of installation steps.

--- configs/emacs.d/packages.el
+++ configs/emacs.d/packages.el
@@ -1,103 +1,10 @@
-;; If we are using an appropriate emacs, install packages from
-;; repositories.
-(add-to-list 'package-archives
-	     '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-	     '("elpa" . "http://tromey.com/elpa/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/"))
+(defvar my-package-list '(use-package)
+  "List of packages to be installed upon initialization.")
 
-
-;; Create a list of packages we want to be installed.
-(setq my-package-list '(ecb
-			highlight-indentation
-			google-c-style
-			hide-comnt
-			paredit
-			company
-			company-math
-			company-irony
-			company-anaconda
-			company-c-headers
-			company-quickhelp
-			rainbow-mode
-			graphviz-dot-mode
-			glsl-mode
-			cmake-mode
-			xclip
-			yasnippet
-			srefactor
-			expand-region
-			gitignore-mode
-			gitconfig-mode
-			ssh-config-mode
-			evil
-			elpy
-			anaconda-mode
-			web-mode
-			dart-mode
-			magit
-			auto-complete
-			ac-etags
-			ac-anaconda
-			auto-complete-clang
-			flycheck
-			flycheck-irony
-			flycheck-pos-tip
-			irony
-			auto-complete-c-headers
-			c-eldoc
-			css-eldoc
-			irony-eldoc
-			ggtags
-			csv-mode
-			cuda-mode
-			coffee-mode
-			projectile
-			clojure-mode
-			clojure-mode-extra-font-locking
-			cider
-			rainbow-delimiters
-			aurora-theme
-			zenburn-theme
-			sublime-themes
-			markdown-mode
-			dropdown-list
-			powershell
-			ace-window
-			ace-jump-mode
-			emms
-			emms-player-mpv
-			form-feed
-			auctex
-			auctex-latexmk
-			auto-complete-auctex
-			company-auctex
-			undo-tree))
-
-
-;; Create an alist of where we should retrieve certain packages.
-(setq package-pinned-packages
-      '((coffee-mode     . "melpa")
-	(form-feed       . "melpa")
-	(emms            . "melpa")
-	(emms-player-mpv . "melpa")
-	(paredit         . "melpa")))
-
-
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
+(package-refresh-contents)
 
 (dolist (package my-package-list)
   (unless (package-installed-p package)
     (condition-case nil
 	(package-install package)
       (error (message "[ERROR]: Failed to install package: %s." package)))))
-
-
-;; Remove no longer needed packages.
-(when (fboundp 'package-autoremove)
-  (package-autoremove))
