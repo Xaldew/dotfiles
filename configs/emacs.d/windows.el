379648114e5ba03d4282ced30d9e3ced0e236222
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-09-20 20:50:45 +0200

    Flyspell now works on Windows in English.
    More languages could be added by changing
    `ispell-hunspell-dictionary-alist` and adding appropriate dictionaries
    to the hunspell share directory. However, finding that directory
    programmatically is a bit of a challenge.

--- configs/emacs.d/windows.el
+++ configs/emacs.d/windows.el
@@ -3,5 +3,29 @@
           (eq system-type 'ms-dos))
   (setq visible-bell t)
   (setq ring-bell-function 'ignore)
+
+  ;; Setup dictionary for using Hunspell with Flyspell-mode.
+  (require 'flyspell)
+  (setq ispell-dictionary "english")
+  (add-to-list 'ispell-local-dictionary-alist '(("svenska"
+                                                 "[[:alpha:]]"
+                                                 "[^[:alpha:]]"
+                                                 "[']"
+                                                 nil
+                                                 ("-d" "sv_SE")
+                                                 t
+                                                 utf-8)
+                                                ("english"
+                                                 "[[:alpha:]]"
+                                                 "[^[:alpha:]]"
+                                                 "[']"
+                                                 t
+                                                 ("-d" "en_US")
+                                                 nil
+                                                 utf-8)))
+  (setq ispell-hunspell-dictionary-alist ispell-local-dictionary-alist)
+
+  ;; Startup the server if not already running.
+  (require 'server)
   (unless (server-running-p)
     (server-start)))
