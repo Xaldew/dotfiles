af37a164eaa2b86c08c3deb778bcefe9e1def3f9
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-10-03 20:32:48 +0200

    LanguageTool is downloaded as an external script.

--- scripts/install_external_scripts.sh
+++ scripts/install_external_scripts.sh
@@ -61,3 +61,22 @@ if [ ! -x $local_prefix_dir/bin/lein ]; then
     wget --quiet $url --output-document=$local_prefix_dir/bin/lein
     chmod u+x $local_prefix_dir/bin/lein
 fi
+
+# Install LanguageTool for spell- and grammarchecking.
+if [ ! -d $local_prefix_dir/bin/languagetool ]; then
+    dest=$local_prefix_dir/bin/languagetool
+    mkdir --parents $dest
+    tmpdir=$(mktemp --directory)
+    url=https://languagetool.org/download/LanguageTool-stable.zip
+    wget --quiet $url --output-document=$tmpdir/languagetool.zip
+    zip=$tmpdir/languagetool.zip
+
+    # Extract zip and strip toplevel directories.
+    unzip -q -d "$dest" "$zip"
+    for f in "$dest"/*;
+    do
+        mv $f/* "$dest"
+        rmdir $f
+    done
+    rm -r $tmpdir
+fi
