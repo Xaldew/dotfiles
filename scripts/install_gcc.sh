c250128bb0e1f9da5966239a81b9530a748b98ba
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-11-18 16:57:52 +0100

    Updated install_gcc.sh.
    * It now intalls the full gcc history to include all tags/releases.
    * Prerequisites are automatically downloaded the first time.
    * apt-get is not used automatically anymore to install libraries.

--- scripts/install_gcc.sh
+++ scripts/install_gcc.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env sh
 # Install the GNU Compiler Collection.
 
-sudo apt-get install \
-     libgmp-dev \
-     libmpfr-dev \
-     libmpc-dev
+# DEB: sudo apt-get install libgmp-dev libmpfr-dev libmpc-dev
 
 cd $objects_dir
 if [ -d $objects_dir/gcc ]; then
     cd $objects_dir/gcc
     git pull
 else
-    git clone --depth 1000 git://gcc.gnu.org/git/gcc.git
+    git clone git://gcc.gnu.org/git/gcc.git
+    git checkout -B v5.2.0 tags/gcc-5_2_0-release
+    $objects_dir/gcc/contrib/contrib/download_prerequisites
 fi
 
 cd $objects_dir/gcc
 ./configure --prefix=$local_prefix_dir \
 	    --mandir=$local_prefix_dir/share/man \
+            --program-suffix=5.2 \
 	    --enable-gold=yes \
 	    --enable-lto
 make -j4
