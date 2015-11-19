2b6ac95fb13b2762d3d7bd8017ef224e46194701
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-11-19 16:45:32 +0100

    Python install script now working.
    * Still need to figure out simplest way to add personal shared
      libraries. So far it seems easiest to simply add an entry to
      ld.so.conf.

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env sh
 # Install Python2.7 and Python3
 
-(
-    url=https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-    if [ ! -d $objects_dir/python27 ]; then
-        tmp=`mktemp --directory`
-        cd $tmp
-        wget --quiet $url
-        rm -r $tmp
-    fi
-) &
 
-(
-    url=https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz
-    if [ ! -d $objects_dir/python3 ]; then
-        tmp=`mktemp --directory`
-        :
-        rm -r $tmp
-    fi
-) &
+url=https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+if [ ! -d $objects_dir/python27 ]; then
+    cd $objects_dir
+    file=`basename $url`
+    wget --quiet $url
+    tar xf $file
+    dir=`basename $file .tgz`
+    rm $file
+    mv $dir python27
+    dir=python27
+    cd $dir
+    ./configure --prefix=$local_prefix_dir --enable-unicode=ucs4 --enable-shared
+    make -j4
+    make altinstall
+fi
+
+url=https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz
+if [ ! -d $objects_dir/python3 ]; then
+    cd $objects_dir
+    file=`basename $url`
+    wget --quiet $url
+    tar xf $file
+    dir=`basename $file .tgz`
+    rm $file
+    mv $dir python3
+    dir=python3
+    cd $dir
+    ./configure --prefix=$local_prefix_dir --enable-shared
+    make -j4
+    make altinstall
+fi
