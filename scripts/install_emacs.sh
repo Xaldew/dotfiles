e6f52695c84c4b4cb2ba315b4c55abe35c10895e
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-12-09 14:11:00 +0100

    install_emacs.sh only installs latest emacs-25.
    Installing Emacs-24 as part of the script made it difficult to maintain
    and recompile.
    
    Now that Emacs-25 is more or less feature-complete, we should only
    install that.
    
    This may change if some compelling feature shows up in Emacs master.

--- scripts/install_emacs.sh
+++ scripts/install_emacs.sh
@@ -1,51 +1,26 @@
 #!/usr/bin/env sh
 # Install the latest development release of Emacs.
 
-sudo apt-get --quiet=2 install \
-     libmagickcore-dev \
-     libmagickwand-dev \
-     libgtk-3-dev \
-     libjpeg-dev \
-     libgif-dev \
-     libtiff-dev \
-     libxpm-dev \
-     libxaw7-dev
+# DEB: sudo apt-get install libmagickcore-dev libmagickwand-dev libgtk-3-dev
+# libjpeg-dev libgif-dev libtiff-dev libxpm-dev libxaw7-dev
 
-cd $objects_dir
-
-if [ -d $objects_dir/emacs ]; then
-    cd $objects_dir/emacs
-    make distclean
-    git clean --force
-    find -name *.elc -type f -print | xargs rm --force
-    find -name *.loaddefs.el* -type f -print | xargs rm
-    git checkout -B master remotes/origin/master
-    git pull
-    git checkout -B emacs-24 remotes/origin/emacs-24
-    git pull
+if [ ! -d $objects_dir/emacs ]; then
+    git clone git://git.savannah.gnu.org/emacs.git $objects_dir/emacs
 else
-    git clone git://git.savannah.gnu.org/emacs.git emacs
+    cd $objects_dir/emacs
+    git pull
 fi
 
-# Compile and install Emacs-24.
+# Compile and install the latest version of Emacs-25.
 cd $objects_dir/emacs
-git checkout -B emacs-24 origin/emacs-24
-sh autogen.sh
-./configure --prefix=$local_prefix_dir \
-	    --mandir=$local_prefix_dir/share/man \
-	    --enable-link-time-optimization \
-	    --with-imagemagick \
-	    --with-x-toolkit=lucid
-make -j4
-make install
-
-
-# Compile and install the latest Emacs.
 make distclean
 git clean --force
-find -name *.elc -type f -print | xargs rm --force
-find -name *.loaddefs.el* -type f -print | xargs rm
-git checkout -B master origin/master
+
+# To do a full clean-up, uncomment the following lines.
+# find -name *.elc -type f -print | xargs rm --force
+# find -name *.loaddefs.el* -type f -print | xargs rm --force
+
+git checkout -B emacs-25 origin/emacs-25
 sh autogen.sh
 ./configure --prefix=$local_prefix_dir \
 	    --mandir=$local_prefix_dir/share/man \
