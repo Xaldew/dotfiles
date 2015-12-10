0e030779c3a7e6867540619738ce1c1091e970b1
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-12-10 21:51:22 +0100

    install_global.sh now downloads latest release.
    Removed dependencies on CVS and git-cvs utilities by downloading the
    latest release instead.

--- scripts/install_global.sh
+++ scripts/install_global.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env sh
 # Install the latest development relase of Emacs.
 
-sudo apt-get --quiet=2 install \
-     bison \
-     flex \
-     gperf
+# DEB: sudo apt-get bison flex gperf
+
 
 cd $objects_dir
-CVSROOT=:pserver:anonymous@cvs.savannah.gnu.org:/sources/global
-if [ -d $objects_dir/global ]; then
-    cd $objects_dir/global
-    git cvsimport -d $CVSROOT -r cvs -v -R global \
-	-A $objects_dir/global/.cvs_authors
-else
-    mkdir -p $objects_dir/global
-    touch $objects_dir/global/.cvs_authors
-    git cvsimport -p "-x" -d $CVSROOT -C global -r cvs -k -z3 -v \
-	-A $objects_dir/global/.cvs_authors -R global
+if [ ! -d $objects_dir/global ]; then
+    url="ftp://ftp.gnu.org/pub/gnu/global/global-6.5.1.tar.gz"
+    file=`basename $url`
+    wget --quiet $url --output-document=$file
+    tar -zxf $file --transform='s/-6.5.1//g'
+    rm $file
 fi
 
 cd $objects_dir/global
@@ -24,6 +18,6 @@ sh reconf.sh
 ./configure \
     --prefix=$local_prefix_dir \
     --mandir=$local_prefix_dir/share/man \
-    --with-exuberant-ctags=ex-ctags
+    --with-exuberant-ctags=uni-ctags
 make -j4
 make install
