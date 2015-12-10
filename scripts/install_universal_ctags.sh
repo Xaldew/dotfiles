7bf836ae6ebb0abf3fc9ffb8b2e0b7cd6bb77ccc
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-12-10 21:41:39 +0100

    Added install_universal_ctags.sh.

---
+++ scripts/install_universal_ctags.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+# Install latest Exuberant ctags and rename it appropriately.
+
+cd $objects_dir
+if [ -d $objects_dir/universal-ctags ]; then
+    cd $objects_dir/universal-ctags
+    git pull
+else
+    git clone \
+        https://github.com/universal-ctags/ctags.git \
+        $objects_dir/universal-ctags
+fi
+
+cd $objects_dir/universal-ctags
+
+./autogen.sh
+./configure \
+    --prefix=$local_prefix_dir \
+    --mandir=$local_prefix_dir/share/man \
+    --program-prefix="uni-" \
+    --disable-etags
+make -j4
+make install
