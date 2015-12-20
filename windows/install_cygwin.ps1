e86a90b88df372e06f0689bf8652b8121b148757
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-12-20 23:42:16 +0100

    Added powershell script to install Cygwin pkgs.

---
+++ windows/install_cygwin.ps1
@@ -0,0 +1,47 @@
+# Install all Cygwin packages used during regular development.
+$scriptPath  = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$dotfilesDir = Split-Path -Parent $scriptPath
+
+# Source common utilities.
+. $dotfilesDir/windows/install_utils.ps1
+
+
+if (!(Test-CommandExists cyg-get))
+{
+    Write-Host "\"cyg-get\" command not found. Please install it first."
+    Exit 1
+}
+
+
+$packages = @(
+    "tmux",
+    "procps",
+    "emacs-w32",
+    "zip",
+    "unzip",
+    "xz",
+    "openssh",
+    "ca-certificates",
+    "git",
+    "subversion",
+    "mercurial",
+    "make",
+    "curl",
+    "wget",
+    "python",
+    "hunspell",
+    "ncurses",
+    "libncurses-devel"
+    "imagemagick",
+    "autoconf",
+    "automake",
+    "cmake",
+    "gcc",
+    "clang",
+    "aspell",
+    "aspell-sv",
+    "aspell-en",
+    "xclip"
+)
+
+cyg-get $packages
