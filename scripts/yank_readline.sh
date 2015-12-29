0438528ae68a3fe500f52bb269a36e3a9c566542
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2015-12-29 12:31:50 +0100

    yank_readline.sh now uses anyclip/anypaste.
    Use the portable wrapper script instead for readline copy&paste.

--- scripts/yank_readline.sh
+++ scripts/yank_readline.sh
@@ -2,35 +2,35 @@
 # Utilities for integrating readline and the X clipboards.
 
 # Verify that xclip is available, and that bash is at least version 4.
-if command -v xclip > /dev/null 2>&1 && [ $BASH_VERSINFO -ge 4 ]; then
+if command -v anyclip > /dev/null 2>&1 && [ $BASH_VERSINFO -ge 4 ]; then
 
-    function _xdiscard()
+    function _cli_discard()
     {
-	echo -n "${READLINE_LINE:0:$READLINE_POINT}" | \
-	    xclip -selection clipboard
-	READLINE_LINE="${READLINE_LINE:$READLINE_POINT}"
+        echo -n "${READLINE_LINE:0:$READLINE_POINT}" | anyclip
+        READLINE_LINE="${READLINE_LINE:$READLINE_POINT}"
 	READLINE_POINT=0
     }
-    function _xkill()
+
+    function _cli_kill()
     {
-	echo -n "${READLINE_LINE:$READLINE_POINT}" | \
-	    xclip -selection clipboard
+        echo -n "${READLINE_LINE:$READLINE_POINT}" | anyclip
 	READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}"
     }
-    function _xyank()
+
+    function _cli_yank()
     {
-	clip=$(xclip -selection clipboard -out)
+        clip=$(anypaste)
 	count=$(echo -n "$clip" | wc -c)
 	READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}"
 	READLINE_LINE+="${clip}${READLINE_LINE:$READLINE_POINT}"
 	READLINE_POINT=$(($READLINE_POINT + $count))
     }
 
-    # Add keybindings M-k/M-k/M-u to discard/kill/yank to/from the clipboard
+    # Add keybindings M-u/M-k/M-y to discard/kill/yank to/from the clipboard
     # through readline.
     if [ -z "$INSIDE_EMACS" ]; then
-	bind -m emacs -x '"\eu": _xdiscard'
-	bind -m emacs -x '"\ek": _xkill'
-	bind -m emacs -x '"\ey": _xyank'
+	bind -m emacs -x '"\eu": _cli_discard'
+	bind -m emacs -x '"\ek": _cli_kill'
+	bind -m emacs -x '"\ey": _cli_yank'
     fi
 fi
