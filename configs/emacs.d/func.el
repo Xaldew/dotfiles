7ccde77ba3661eb77799cb3be1194fd18f6b76b3
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-01-01 14:21:02 +0100

    Emacs: func.el now follows Elisp coding practices.

--- configs/emacs.d/func.el
+++ configs/emacs.d/func.el
@@ -1,5 +1,13 @@
-;; This file contains various useful functions and aliases.
-(eval-when-compile (require 'rect))
+;;; func.el --- Provide various utilities and aliases.
+;;
+;;; Commentary:
+;; This file contains various useful functions and aliases. That I use
+;; frequently in everyday coding.
+;;
+;;; Code:
+
+(declare-function apply-on-rectangle "rect")
+
 
 (defun indent-defun ()
   "Indent the currently active defun."
@@ -13,7 +21,9 @@
 
 
 (defun indent-whole-buffer ()
-  "Indent the whole buffer, fixing indentation and removes whitespace."
+  "Indent the whole buffer.
+
+Fixes indentation and removes erroneous whitespace."
   (interactive)
   (delete-trailing-whitespace)
   (indent-region (point-min) (point-max) nil))
@@ -36,13 +46,16 @@
 
 
 (defun hardcopy ()
-  "Make a hardcopy of the current buffer. I.e., print it."
+  "Make a hardcopy of the current buffer.
+
+I.e., print the buffer.  Attempts to keep as much of the
+indentation style and syntax highlighting as possible."
   (interactive)
   (ps-print-buffer-with-faces))
 
 
 (defun dos2unix (buffer)
-  "Convert the coding system to unix style line-endings."
+  "Convert the BUFFER coding system to Unix style line-endings."
   (interactive "*b")
   (save-excursion
     (goto-char (point-min))
@@ -51,39 +64,39 @@
   (set-buffer-file-coding-system 'utf-8-unix 't))
 
 
-(defun eol-append (str p0 p1)
-  "prompt for string, add it to end of lines in the region"
+(defun eol-append (string p0 p1)
+  "Prompt for STRING and add it to end of lines in the region [P0,P1]."
   (interactive "sWhat shall we append? \nr")
   (goto-char p1)
   (forward-line -1)
   (while (>= (point) p0)
     (end-of-line)
-    (insert str)
+    (insert string)
     (forward-line -1)))
 
 
-(defun upcase-rectangle (b e)
-  "Change all characters in a rectangle to uppercase."
+(defun upcase-rectangle (beg end)
+  "Change all characters in a rectangle between BEG and END to uppercase."
   (interactive "r")
-  (apply-on-rectangle 'upcase-rectangle-line b e))
+  (apply-on-rectangle 'upcase-rectangle-line beg end))
 
 
 (defun upcase-rectangle-line (startcol endcol)
-  "Change all characters on a line to uppercase."
+  "Change all characters on a line from STARTCOL to ENDCOL to uppercase."
   (when (= (move-to-column startcol) startcol)
     (upcase-region (point)
 		   (progn (move-to-column endcol 'coerce)
 			  (point)))))
 
 
-(defun downcase-rectangle (b e)
-  "Change all characters in a rectangle to uppercase."
+(defun downcase-rectangle (beg end)
+  "Change all characters in a rectangle between BEG and END to lowercase."
   (interactive "r")
-  (apply-on-rectangle 'downcase-rectangle-line b e))
+  (apply-on-rectangle 'downcase-rectangle-line beg end))
 
 
 (defun downcase-rectangle-line (startcol endcol)
-  "Change all characters on a line to uppercase."
+  "Change all characters on a line from STARTCOL to ENDCOL to lowercase."
   (when (= (move-to-column startcol) startcol)
     (downcase-region (point)
 		     (progn (move-to-column endcol 'coerce)
@@ -96,15 +109,21 @@
   (byte-recompile-directory user-emacs-directory 0))
 
 
-(defun locate-directory (file-name path &optional suffix predicate)
-  "Locate the given directory in the list of directories.
+(defun locate-directory (dir-name path &optional suffix predicate)
+  "Locate the directory DIR-NAME in the list of directories in PATH.
 
-All arguments are treated similar to to the `locate-file' function.
+Optional argument SUFFIX is a list of valid directory suffixes.
 
-"
+Optional argument PREDICATE is a function to test the directory
+with prior to accepting it.
+
+All arguments are treated similar to to the `locate-file' function."
   (locate-file
-   file-name path suffix
+   dir-name path suffix
    (lambda (f)
      (when (and (file-directory-p f)
                 (or (not predicate) (funcall predicate f)))
        'dir-ok))))
+
+
+;;; func.el ends here
