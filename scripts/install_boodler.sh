123fb55a3ba5a972b0dfa806b67bff978ba5368c
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-01-03 20:24:50 +0100

    Emacs: install_boodler.sh fixes for Unix.
    Fix for building Boodler when running on Unix-like OSes.
    
    Only download the .boop-files the first time.

--- scripts/install_boodler.sh
+++ scripts/install_boodler.sh
@@ -7,13 +7,23 @@ tar xf Boodler-2.0.4.tar.gz
 rm Boodler-2.0.4.tar.gz
 cd Boodler-2.0.4
 python setup.py build
+
+if [ "$OSTYPE" != "cygwin" ]; then
+    printf "[build_scripts]\n" >> setup.cfg
+    printf "default_driver=pulse\n" >> setup.cfg
+fi
+
 python setup.py install --prefix $local_prefix_dir
 
-mkdir --parents $HOME/.boodler/Download
-cd $HOME/.boodler/Download
-wget --no-directories --quiet --recursive --no-parent --accept "*.boop" \
-     http://boodler.org/lib/
+# Download all ".boop"-files from.
+if [ ! -d $HOME/.boodler/Download ]; then
+    mkdir --parents $HOME/.boodler/Download
+    cd $HOME/.boodler/Download
+    wget --no-directories --quiet --recursive --no-parent --accept "*.boop" \
+         http://boodler.org/lib/
 
-for f in *.boop; do
-    boodle-mgr install $f > /dev/null
-done
+    for f in *.boop; do
+        printf "Install: %s\n" $f
+        boodle-mgr install $f
+    done
+fi
