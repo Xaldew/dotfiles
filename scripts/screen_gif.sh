d26d86ba15d9c58f5be9e1ccfd165e5dbca7fabc
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-01-18 23:38:02 +0100

    Scripts: Added screen_gif.sh.
    This scripts easily generates a short GIF over a region of the screen.

--- scripts/screen_gif.sh
+++ scripts/screen_gif.sh
@@ -6,3 +6,4 @@ ffcast -s % ffmpeg -y -f x11grab -show_region 1 -framerate 15 \
        -video_size %s -i %D+%c -codec:v huffyuv               \
        -vf crop="iw-mod(iw\\,2):ih-mod(ih\\,2)" $tmp_avi      \
     && convert -set delay 10 -layers Optimize $tmp_avi out.gif
+rm $tmp_avi
