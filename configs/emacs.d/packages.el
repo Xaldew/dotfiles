25678232d44ddd656bed3690e012ed5bb00c1009
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-01-26 14:27:01 +0100

    Emacs: Added header and footer in packages.el.

--- configs/emacs.d/packages.el
+++ configs/emacs.d/packages.el
@@ -1,3 +1,10 @@
+;;; packages.el --- Install `use-package' if not already installed.
+;;
+;;; Commentary:
+;; This package is responsible for installing use-package if not already done.
+;;
+;;; Code:
+
 (defvar my-package-list '(use-package)
   "List of packages to be installed upon initialization.")
 
@@ -8,3 +15,5 @@
     (condition-case nil
 	(package-install package)
       (error (message "[ERROR]: Failed to install package: %s." package)))))
+
+;;; packages.el ends here
