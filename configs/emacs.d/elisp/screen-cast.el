80727d3e51f19c79a5ee293418ce3f07bb4c7363
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-02-17 16:47:13 +0100

    ScreenCast.el: Emacs plugin mostly working.

--- configs/emacs.d/elisp/screen-cast.el
+++ configs/emacs.d/elisp/screen-cast.el
@@ -14,13 +14,66 @@
 (defvar screen-cast-clm/logging-dir nil
   "Old value of `clm/logging-dir'.")
 
-(defun screen-cast ()
-  "Record a screen-cast with Emacs keys and actions recorded."
+(defvar screen-cast-clm-on nil
+  "Was `command-log-mode' active when starting the screen-cast?.")
+
+(defvar screen-cast-tmp-dir nil
+  "Temporary directory were the current screen cast data is saved.")
+
+(defvar screen-cast-output nil
+  "The name of the file output screen-cast file.")
+
+(defvar screen-cast-start-time nil
+  "The start time of the last screen-cast.")
+
+
+(defun screen-cast-sentinel (process event)
+  "Process sentinel for the screen-cast.
+
+PROCESS: The process that received EVENT."
+  (cond
+   ((equal event "finished\n")
+    (unless screen-cast-clm-on
+      (command-log-mode -1))
+    (setq clm/logging-dir screen-cast-clm/logging-dir)
+    (setq command-log-mode-is-global screen-cast-clm-mode-global)
+    (when clm/command-log-buffer
+      (with-current-buffer clm/command-log-buffer
+        (print (buffer-substring (point-min) (point-max)))))
+    (delete-directory screen-cast-tmp-dir t nil))
+   (t
+    (princ (format "Process: %s had the event `%s'" process event)))))
+
+
+(defun screen-cast (output-file)
+  "Record a screen-cast with Emacs keys and actions recorded.
+
+Output screen-cast GIF is saved to OUTPUT-FILE."
+  (interactive "F")
+  (setq screen-cast-clm-on (member 'command-log-mode minor-mode-list))
+  (setq screen-cast-clm-logging-dir clm/logging-dir)
+  (setq screen-cast-clm-mode-global command-log-mode-is-global)
+
+  (setq screen-cast-start-time (current-time))
+  (setq screen-cast-output output-file)
+  (setq screen-cast-tmp-dir (make-temp-file "screen-cast-" 'dir))
+  (setq clm/logging-dir screen-cast-tmp-dir)
+  (setq command-log-mode-is-global t)
   (command-log-mode t)
-  (let ((tmp-dir (make-temp-file "screen-cast" 'dir)))
-    (start-process "screen-cast" "*screen-cast*"
-                   "screen_cast.py"
-                   "--record-keys"
-                   "--save-temporaries" tmp-dir)))
+  (save-window-excursion
+    (clm/open-command-log-buffer))
+  (let* ((output-gif (concat (file-name-as-directory screen-cast-tmp-dir)
+                             "out.gif"))
+         (process (start-process "screen-cast"
+                                 "*screen-cast*"
+                                 "screen_cast.py"
+                                 "--record-keys"
+                                 "--save-intermediates"
+                                 screen-cast-tmp-dir
+                                 "--output-file" output-gif)))
+    (set-process-sentinel process 'screen-cast-sentinel)))
+
+
+(provide 'screen-cast)
 
 ;;; screen-cast.el ends here
