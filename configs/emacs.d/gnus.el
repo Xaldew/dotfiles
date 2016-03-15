0bd6f2121cb52832f891f32e0eccbb322479a850
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-03-15 23:08:50 +0100

    Emacs: Added gnus.el configuration.

---
+++ configs/emacs.d/gnus.el
@@ -0,0 +1,41 @@
+;;; gnus.el -- Personal Emacs GNUS configuration.
+;;
+;;; Commentary:
+;; Personal settings for using GNUS to connect to various mail clients.
+;;
+;;; Code:
+
+(setq gnus-select-method
+      '(nnimap "gmail"
+	       (nnimap-address "imap.gmail.com")
+	       (nnimap-server-port "imaps")
+	       (nnimap-stream ssl)))
+
+(add-to-list 'gnus-secondary-select-methods
+             '(nnimap "ARM"
+                      (nnimap-address "outlook.office365.com")
+                      (nnimap-server-port "imaps")
+                      (nnimap-stream tls)
+                      (nnir-search-engine imap)))
+
+(setq smtpmail-smtp-service 587
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+
+;; Always display nnimap mails in topic view.
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+
+(setq gnus-posting-styles
+      '((".*"
+         (signature "Written at home from %s." (emacs-version))
+         (name "Gustaf Waldemarson")
+         ("X-Message-SMTP-Method" "smtp smtp.gmail.com 587")
+         ;; (x-face-file "~/.xface")
+         ;; (x-url (getenv "WWW_HOME"))
+         )
+        ((posting-from-work-p) ;; A user defined function
+         (signature "Written at work from %s." (emacs-version))
+         (address "gustaf.waldemarson@arm.com")
+         ("X-Message-SMTP-Method" "smtp smtp.office365.com 587")
+         (organization "ARM Ltd."))))
+
+;;; gnus.el ends here
