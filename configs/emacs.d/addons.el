12aa92628c30780d81fd524167056ce782322317
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-03-17 16:37:06 +0100

    Emacs: Added mmm-mode package configuration.
    Added python-mode + rst-mode docstring configurations.

--- configs/emacs.d/addons.el
+++ configs/emacs.d/addons.el
@@ -519,6 +519,41 @@
   (setq browse-kill-ring-highlight-current-entry t))
 
 
+(use-package mmm-mode
+  :ensure t
+  :init
+  (add-hook 'python-mode-hook 'mmm-mode)
+  :config
+  (defun rst-python-statement-is-docstring (begin)
+    "Return true if beginning of statement is BEGIN."
+    (save-excursion
+      (save-match-data
+        (python-nav-beginning-of-statement)
+        (looking-at-p begin))))
+
+  (defun rst-python-front-verify ()
+    "Verify that we're looking at a python docstring."
+    (rst-python-statement-is-docstring (match-string 0)))
+
+  (setq mmm-parse-when-idle 't)
+  (add-to-list 'mmm-save-local-variables 'adaptive-fill-regexp)
+  (add-to-list 'mmm-save-local-variables 'fill-paragraph-function)
+
+  (mmm-add-classes
+   '((rst-python-docstrings
+      :submode rst-mode
+      :face mmm-comment-submode-face
+      :front "u?\\(\"\"\"\\|\'\'\'\\)"
+      :front-verify rst-python-front-verify
+      :back "~1"
+      :end-not-begin t
+      :save-matches 1
+      :insert ((?d embdocstring nil @ "u\"\"\"" @ _ @ "\"\"\"" @))
+      :delimiter-mode nil)))
+
+  (mmm-add-mode-ext-class 'python-mode nil 'rst-python-docstrings))
+
+
 ;; Install various major-mode packages and defer where it is possible.
 (use-package graphviz-dot-mode :ensure t :defer t)
 (use-package glsl-mode         :ensure t :defer t)
