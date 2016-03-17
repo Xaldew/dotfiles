061f92ed90736891b79576fd1ca711a478962cd6
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-03-17 16:40:58 +0100

    Emacs::gnus: Fixed `my-posting-from-work` defun.

--- configs/emacs.d/gnus.el
+++ configs/emacs.d/gnus.el
@@ -24,6 +24,11 @@
 ;; Always display nnimap mails in topic view.
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 
+(defun my-posting-from-work-p ()
+  "Check if we are posting this GNUS message at work."
+  (or (string-match-p "e[0-9]\\{6\\}-lin" (system-name))
+      (string= (user-login-name) "guswal01")))
+
 (setq gnus-posting-styles
       '((".*"
          (signature "Written at home from %s." (emacs-version))
@@ -32,7 +37,7 @@
          ;; (x-face-file "~/.xface")
          ;; (x-url (getenv "WWW_HOME"))
          )
-        ((posting-from-work-p) ;; A user defined function
+        ((my-posting-from-work-p) ;; A user defined function
          (signature "Written at work from %s." (emacs-version))
          (address "gustaf.waldemarson@arm.com")
          ("X-Message-SMTP-Method" "smtp smtp.office365.com 587")
