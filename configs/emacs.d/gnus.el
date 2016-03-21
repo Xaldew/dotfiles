7b63a382a801ea46f5dcab5a3ec87612278d9d25
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-03-21 23:20:16 +0100

    Emacs::gnus: Prettyfied summary lines and threads.

--- configs/emacs.d/gnus.el
+++ configs/emacs.d/gnus.el
@@ -43,4 +43,27 @@
          ("X-Message-SMTP-Method" "smtp smtp.office365.com 587")
          (organization "ARM Ltd."))))
 
+
+(when window-system
+  (setq gnus-sum-thread-tree-indent "  ")
+  (setq gnus-sum-thread-tree-root "● ")
+  (setq gnus-sum-thread-tree-false-root "◯ ")
+  (setq gnus-sum-thread-tree-single-indent "◎ ")
+  (setq gnus-sum-thread-tree-vertical        "│")
+  (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
+  (setq gnus-sum-thread-tree-single-leaf     "╰─► "))
+
+(setq gnus-summary-line-format
+      (concat
+       "%0{%U%R%z%}"
+       "%3{│%}" "%1{%d%}" "%3{│%}"  ; Date
+       "  "
+       "%4{%-20,20f%}"              ; Name
+       "  "
+       "%3{│%}"
+       " "
+       "%1{%B%}"
+       "%s\n"))
+(setq gnus-summary-display-arrow t)
+
 ;;; gnus.el ends here
