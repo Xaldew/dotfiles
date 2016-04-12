48ebaba04e98e787667ef9062b3bb66000cb6518
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-04-12 14:55:50 +0200

    Emacs: Added ipython interpreter settings.

--- configs/emacs.d/pymode.el
+++ configs/emacs.d/pymode.el
@@ -1,6 +1,20 @@
-;; Python specific settings.
+;;; pymode.el --- Personal Python specific settings.
+;;
+;;; Commentary:
+;; Personal Python mode specific settings.
+;;
+;;; Code:
+
 (defun my/python-mode-hook ()
-  "My python mode hook."
+  "Personal Python-mode hook for buffer local settings."
+  (when (executable-find "ipython")
+    (setq
+     python-shell-interpreter "ipython"
+     python-shell-interpreter-args ""
+     python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+     python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+     python-shell-setup-codes '())) ; TODO: Add autoload setup code.
+
   (define-key python-mode-map [f5] 'python-check)
   (define-key python-mode-map [f7] 'pdb)
   (define-key python-mode-map [f8] 'run-python)
@@ -12,3 +26,5 @@
 				 indentation
 				 indentation::space)))
 (add-hook 'python-mode-hook #'my/python-mode-hook)
+
+;;; pymode.el ends here
