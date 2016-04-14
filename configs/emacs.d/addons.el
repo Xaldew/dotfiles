77228df882ca63e38479f8f5dc45283867fb8976
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-04-14 16:42:56 +0200

    Emacs::gnus/hydras: Added various Gnus hydras.

--- configs/emacs.d/addons.el
+++ configs/emacs.d/addons.el
@@ -679,6 +679,76 @@ _h_   _l_  _r_: Reset     _p_: Paste   _C_:  Clear       _sr_: String Replace  _
   (global-set-key (kbd "C-x SPC") 'hydra-rectangle/body))
 
 
+;; Gnus addons configurations.
+(use-package message
+  :defer t
+  :after hydra
+  :config
+  (defhydra hydra-message (:color blue)
+    "Do?"
+    ("ca" mml-attach-file "Attach C-c C-a")
+    ("cc" message-send-and-exit "Send C-c C-c")
+    ("q" nil "cancel")))
+
+
+(use-package gnus-group
+  :defer t
+  :after hydra
+  :config
+  ;; y is not used by default
+  (defhydra hydra-gnus-group (:color blue)
+    "Do?"
+    ("a" gnus-group-list-active "REMOTE groups A A")
+    ("l" gnus-group-list-all-groups "LOCAL groups L")
+    ("c" gnus-topic-catchup-articles "Read all c")
+    ("G" gnus-group-make-nnir-group "Search server G G")
+    ("g" gnus-group-get-new-news "Refresh g")
+    ("s" gnus-group-enter-server-mode "Servers")
+    ("m" gnus-group-new-mail "Compose m OR C-x m")
+    ("#" gnus-topic-mark-topic "mark #")
+    ("q" nil "cancel"))
+  (define-key gnus-group-mode-map "y" 'hydra-gnus-group/body))
+
+
+(use-package gnus-sum    ;; gnus-summary-mode
+  :defer t
+  :after hydra
+  :config
+  ;; y is not used by default
+  (defhydra hydra-gnus-summary (:color blue)
+    "Do?"
+    ("n" gnus-summary-insert-new-articles "Refresh / N")
+    ("f" gnus-summary-mail-forward "Forward C-c C-f")
+    ("!" gnus-summary-tick-article-forward "Mail -> disk !")
+    ("p" gnus-summary-put-mark-as-read "Mail <- disk")
+    ("c" gnus-summary-catchup-and-exit "Read all c")
+    ("e" gnus-summary-resend-message-edit "Resend S D e")
+    ("R" gnus-summary-reply-with-original "Reply with original R")
+    ("r" gnus-summary-reply "Reply r")
+    ("W" gnus-summary-wide-reply-with-original "Reply all with original S W")
+    ("w" gnus-summary-wide-reply "Reply all S w")
+    ("#" gnus-topic-mark-topic "mark #")
+    ("q" nil "cancel"))
+  (define-key gnus-summary-mode-map "y" 'hydra-gnus-summary/body))
+
+
+(use-package gnus-art   ;; gnus-article-mode
+  :defer t
+  :after hydra
+  :config
+  ;; y is not used by default
+  (defhydra hydra-gnus-article (:color blue)
+    "Do?"
+    ("f" gnus-summary-mail-forward "Forward")
+    ("R" gnus-article-reply-with-original "Reply with original R")
+    ("r" gnus-article-reply "Reply r")
+    ("W" gnus-article-wide-reply-with-original "Reply all with original S W")
+    ("o" gnus-mime-save-part "Save attachment at point o")
+    ("w" gnus-article-wide-reply "Reply all S w")
+    ("q" nil "cancel"))
+  (define-key gnus-article-mode-map "y" 'hydra-gnus-article/body))
+
+
 ;; Install various major-mode packages and defer where it is possible.
 (use-package graphviz-dot-mode :ensure t :defer t)
 (use-package glsl-mode         :ensure t :defer t)
