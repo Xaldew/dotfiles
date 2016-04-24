46ccbf2e5043f384277d82d06a4bdb636fc57972
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-04-24 23:21:42 +0200

    Cygwin: Fix Growl-for-Windows if installed.

--- windows/setup_cygwin.sh
+++ windows/setup_cygwin.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 # Perform post-installation configuration for Cygwin.
-printf "\ndb_home: /%%H\n" >> /etc/nsswitch.conf
+
+if ! grep -Fxq "db_home: /%H" /etc/nsswitch.conf; then
+    printf "\ndb_home: /%%H\n" >> /etc/nsswitch.conf
+fi
+
+growl="$HOME/AppData/Local/Growl/2.0.0.0/user.config"
+if [ -f  "$growl" ]; then
+    sed -i -e 's|\(.*<value>\)Alt+X\(</value>\)|\1Alt+Shift+Y\2|g' $growl
+fi
