2823eaf4650be99c716d64d2cf1b83be09247359
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-09-04 18:45:23 +0200

    Emacs::helm-dash: Added docsets for various modes.

--- configs/emacs.d/addons.el
+++ configs/emacs.d/addons.el
@@ -1491,7 +1491,11 @@ When `ERC' exits the SSH process is killed from `erc-kill-server-hook'."
   :defer t
   :pin melpa
   :init
-  (setq helm-dash-common-docsets '("Python3")))
+  (set-variable-in-hook python-mode-hook  helm-dash-docsets '("Python 3"))
+  (set-variable-in-hook rust-mode-hook    helm-dash-docsets '("Rust"))
+  (set-variable-in-hook shell-script-hook helm-dash-docsets '("Bash"))
+  (set-variable-in-hook c-mode-hook       helm-dash-docsets '("C"))
+  (set-variable-in-hook c++-mode-hook     helm-dash-docsets '("C++" "C")))
 
 
 (use-package ruby-mode
