26bf90140b284d53558920c4e4d24ebfe33e435f
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-09-08 12:35:54 +0200

    Scripts: Bugfixes for `screen_gif.sh`.
    With large files, `imagemagick` would hand over processing to ffmpeg to
    convert the video to a GIF. However, the command it used turned out to
    be outdated, which caused the program to crash.
    
    This fix removes `imagemagick` altogether, replacing it with a few
    optimized ffmpeg commands.
    
    Also, should the recording fail, the temporary files will remain.

--- scripts/screen_gif.sh
+++ scripts/screen_gif.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 # Record a selected screen region and output it as a GIF.
 
-tmp_avi=$(mktemp /tmp/outXXXXXXXXXX.avi)
+out=${1:-out.gif}
+avi=$(mktemp XXXX_XXXX.avi)
 ffcast -s % ffmpeg -y -f x11grab -show_region 1 -framerate 15 \
        -video_size %s -i %D+%c -codec:v huffyuv               \
-       -vf crop="iw-mod(iw\\,2):ih-mod(ih\\,2)" $tmp_avi      \
-    && convert -set delay 10 -layers Optimize $tmp_avi out.gif
-rm $tmp_avi
+       -vf crop="iw-mod(iw\\,2):ih-mod(ih\\,2)" $avi
+
+if [ $? -eq 0 ]; then
+    palette=$(mktemp XXXX_XXXX.png)
+    ffmpeg -y -loglevel error -i $avi -filter:v palettegen $palette
+    ffmpeg -y -loglevel error -i $palette -i $avi \
+           -filter_complex "[1:v][0:v] paletteuse" $out
+    rm $avi $palette
+fi
