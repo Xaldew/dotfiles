e22cd2d738509dbea5e2f092e8fdcc3eff8ea7f4
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-09-19 22:26:56 +0200

    Added PATH to Rust binaries installed by Cargo.
    Updated Rust install script to also install some Rust utilities and
    download the Rust source code.
    
    Added export of RUST_SRC_PATH.

--- scripts/install_rust.sh
+++ scripts/install_rust.sh
@@ -7,3 +7,16 @@ if ! command -v rustc >/dev/null 2>&1; then
     sh $tmp_dir/rustup.sh
     rm -r $tmp_dir
 fi
+
+# Install various Rust utilities.
+if command -v cargo; then
+    cargo install racer
+    cargo install rustfmt
+fi
+
+# Download the Rust source for code completion with Racer.
+if [ ! -d $objects_dir/rust ]; then
+    git clone https://github.com/rust-lang/rust.git $objects_dir/rust
+else
+    git -C $objects_dir/rust pull
+fi
