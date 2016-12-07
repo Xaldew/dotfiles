15d7b7fb89f21463e0c6499933e8bbc3b17b21e6
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-12-07 13:01:27 +0100

    Emacs::kll: Fixed capability regexp.
    The capability regexp was using the wrong regexp specifier (+ -> *).

--- configs/emacs.d/elisp/kll-mode.el
+++ configs/emacs.d/elisp/kll-mode.el
@@ -82,7 +82,7 @@
            (group-n 1 (+ (in "A-Z" "a-z" "0-9" "_")))
            (* space) "=>" (* space)
            (group-n 2 (+ (in "A-Z" "a-z" "0-9" "_")))
-           (* space) "(" (group-n 3 (+ anything)) ")" (* space)
+           (* space) "("(group-n 3 (* anything)) ")" (* space)
            ";"))
   "Regular expression to match KLL capabilities.")
 
