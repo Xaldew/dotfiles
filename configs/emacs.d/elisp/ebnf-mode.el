b76666e5efbfe8fa52674bb66b562fd9cd89a7fc
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-12-12 16:59:23 +0100

    Emacs::ebnf: Added support for multiple styles.
    There are a lot of EBNF styles, but now it's fairly easy to add new ones.

--- configs/emacs.d/elisp/ebnf-mode.el
+++ configs/emacs.d/elisp/ebnf-mode.el
@@ -14,6 +14,15 @@
   :version "25.1")
 
 
+(defcustom ebnf-mode-style 'iso-ebnf
+  "The EBNF style to associate with the current buffer."
+  :group 'ebnf-mode
+  :type '(radio :tag "Style"
+		(const ebnf)
+                (const iso-ebnf))
+  :safe #'symbolp)
+
+
 ;;;; Fontification.
 
 
@@ -40,7 +49,10 @@
   :safe #'integerp)
 
 
-(defconst ebnf-mode-syntax-table
+;;;; `iso-ebnf' style rules.
+
+
+(defconst ebnf-mode-iso-ebnf-syntax-table
   (let ((table (make-syntax-table)))
     (modify-syntax-entry ?'  "\""  table) ; String delimiter
     (modify-syntax-entry ?\" "\"" table)  ; String delimiter
@@ -50,7 +62,7 @@
     table))
 
 
-(defvar ebnf-mode-smie-grammar
+(defvar ebnf-mode-smie-iso-ebnf-grammar
   (smie-prec2->grammar
    (smie-bnf->prec2
     '((id)
@@ -65,9 +77,8 @@
   "Simplfied BNF grammar for `smie' indentation of the `iso-ebnf' style.")
 
 
-(defun ebnf-mode-smie-rules (kind token)
+(defun ebnf-mode-smie-iso-ebnf-rules (kind token)
   "Perform indentation of KIND on TOKEN using the `smie' engine."
-  (message "%s" (cons kind token))
   (pcase (cons kind token)
     (`(:elem . basic)            ebnf-mode-indent-offset)
     (`(:elem . args)             ebnf-mode-indent-offset)
@@ -76,19 +87,83 @@
     (`(:after . "=") ebnf-mode-indent-offset)))
 
 
+;;;; `ebnf' style rules.
+
+
+(defconst ebnf-mode-ebnf-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?'  "\"" table)  ; String delimiter
+    (modify-syntax-entry ?\" "\"" table)  ; String delimiter
+    (modify-syntax-entry ?\; "<"  table)  ; Comment start
+    (modify-syntax-entry ?\n ">"  table)  ; Comment end
+    (modify-syntax-entry ??  "$"  table)  ; paired delimiter
+    table))
+
+
+(defvar ebnf-mode-smie-ebnf-grammar
+  (smie-prec2->grammar
+   (smie-bnf->prec2
+    '((id)
+      (lhs (id))
+      (alts (rhs "||" rhs)
+            (rhs))
+      (rhs ("{" alts "}")
+           (rhs "|" rhs)
+           (id))
+      (rule (lhs "=" rhs))
+      (rules (rules "." rules) (rule)))
+    '((assoc "."))
+    '((assoc "||") (assoc "|"))))
+  "Simplfied BNF grammar for `smie' indentation of the `ebnf' style.")
+
+
+(defun ebnf-mode-smie-ebnf-rules (kind token)
+  "Perform indentation of KIND on TOKEN using the `smie' engine."
+  (pcase (cons kind token)
+    (`(:elem . basic)            ebnf-mode-indent-offset)
+    (`(:elem . args)             ebnf-mode-indent-offset)
+    (`(:list-intro . "=")        0)
+    (`(:before . ,(or "." "|" "||"))  (smie-rule-separator kind))
+    (`(:after . "=") ebnf-mode-indent-offset)))
+
+
 ;;;; Major mode definitions.
 
-(define-derived-mode ebnf-mode prog-mode "EBNF"
-  :group 'ebnf-mode
-  :syntax-table ebnf-mode-syntax-table
+
+(defun ebnf-iso-ebnf-setup ()
+  "Setup the `major-mode' for the `iso-ebnf' style."
   (setq-local comment-start "(* ")
   (setq-local comment-end   " *)")
   (setq-local comment-start-skip "\\((\\*\\)\\s-*")
   (setq-local comment-end-skip   "\\s-*\\(\\*)\\)")
+  (set-syntax-table ebnf-mode-iso-ebnf-syntax-table)
+  (smie-setup ebnf-mode-smie-iso-ebnf-grammar #'ebnf-mode-smie-iso-ebnf-rules))
+
+
+(defun ebnf-ebnf-setup ()
+  "Setup the `major-mode' for the `ebnf' style."
+  (setq-local comment-start "; ")
+  (setq-local comment-start-skip ";+\\s-*")
+  (set-syntax-table ebnf-mode-ebnf-syntax-table)
+  (smie-setup ebnf-mode-smie-ebnf-grammar #'ebnf-mode-smie-ebnf-rules))
+
+
+(defun ebnf-mode-after-hack-local-variables ()
+  "Set EBNF style specific settings after parsing file-local variables."
+  (pcase (list ebnf-mode-style)
+    (`(ebnf)      (ebnf-ebnf-setup))
+    (`(iso-ebnf)  (ebnf-iso-ebnf-setup))
+    (`(,_)        (ebnf-iso-ebnf-setup))))
+
+
+(define-derived-mode ebnf-mode prog-mode "EBNF"
+  :group 'ebnf-mode
+  (print ebnf-mode-style)
   (setq-local indent-tabs-mode nil)
-  (smie-setup ebnf-mode-smie-grammar #'ebnf-mode-smie-rules)
   (setq-local font-lock-defaults ebnf-mode-font-lock-keywords)
-  (font-lock-flush))
+  (font-lock-flush)
+  (add-hook 'hack-local-variables-hook
+            #'ebnf-mode-after-hack-local-variables nil t))
 
 
 ;;;###autoload
