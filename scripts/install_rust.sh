859a9d69b4608466d7032619ef06553024e7c7f9
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2017-01-07 20:57:31 +0100

    Rust: Updated URL to `rustup`.

--- scripts/install_rust.sh
+++ scripts/install_rust.sh
@@ -2,10 +2,7 @@
 
 # Install Rust compiler, runtime and packaging tools.
 if ! command -v rustc >/dev/null 2>&1; then
-    tmp_dir=`mktemp --directory`
-    curl -sSf https://static.rust-lang.org/rustup.sh > $tmp_dir/rustup.sh
-    sh $tmp_dir/rustup.sh
-    rm -r $tmp_dir
+    curl -sSf https://sh.rustup.rs | sh
 fi
 
 # Install various Rust utilities.
