fcc616517cce620b839fa0d11f2dc1845d902aed
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2017-01-10 20:41:30 +0100

    Rust: Installer improvements.
    Now installs nightly toolchain.
    
    Now installs bash completions.

--- scripts/install_rust.sh
+++ scripts/install_rust.sh
@@ -5,11 +5,25 @@ if ! command -v rustc >/dev/null 2>&1; then
     curl -sSf https://sh.rustup.rs | sh
 fi
 
+
 # Install various Rust utilities.
 if command -v cargo; then
     cargo install racer
     cargo install rustfmt
 fi
+
+
+# Use rustup to install setup Rust.
+if command -v rustup; then
+
+    rustup install nightly
+
+    # Install bash completion file.
+    tmp=$(mktemp)
+    rustup completions bash > $tmp
+    sudo mv $tmp /etc/bash_completion.d/rustup.bash-completion
+fi
+
 
 # Download the Rust source for code completion with Racer.
 if [ ! -d $objects_dir/rust ]; then
