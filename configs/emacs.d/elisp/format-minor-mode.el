1d7c77f88f10b96af08ffa758847e4b7367b84ec
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2017-02-24 17:00:29 +0100

    Emacs: Added more matchers to `format-minor-mode`.

--- configs/emacs.d/elisp/format-minor-mode.el
+++ configs/emacs.d/elisp/format-minor-mode.el
@@ -57,6 +57,37 @@
   "Regular expression to match printf-style format specifiers.")
 
 
+(defconst format-minor--shell-var-rx
+  (rx (group-n 1 "$")
+      (or
+       (group-n 2 (in "$*@#!?_-"))
+       (and (group-n 2 (+ (or word "_"))))
+       (and "{#" (group-n 2 (+ (or word "_"))) (? "[" (+? nonl) "]") "}")
+       (and "{"  (group-n 2 (+ (or word "_"))) (? "[" (+? nonl) "]") "}")
+       (and "{"
+            (group-n 2 (+ (or word "_"))) (? "[" (+? nonl) "]")
+            (or "-" "=" "+" "?" ":-" ":=" ":+" ":?")
+            (*? nonl)
+            "}")
+       (and "{"
+            (group-n 2 (+ (or word "_"))) (? "[" (+? nonl) "]")
+            (or ":" "#" "##" "%" "%%") (*? nonl)
+            "}")
+       (and "{"
+            (group-n 2 (+ (or word "_"))) (? "[" (+? nonl) "]")
+            (or "/" "//" "/#" "/%")
+            (*? nonl)
+            (or "/")
+            (*? nonl)
+            "}")
+       (and "{"
+            (group-n 2 (+ (or word "_"))) (? "[" (+? nonl) "]")
+            ":" (*? nonl) ":" (*? nonl)
+            "}")
+       (and "{!" (group-n 2 (+ (or word "_"))) (? (in "*@")) "}")))
+  "Regular expression to match shell-variables inside strings.")
+
+
 (defun format-minor--format-matcher (end)
   "Search for `printf' format specifiers within strings up to END."
   (let ((pos)
@@ -75,20 +106,57 @@
     pos))
 
 
-(defvar format-minor-font-lock-keywords
+(defun format-minor--shell-matcher (end)
+  "Search for `printf' format specifiers within strings up to END."
+  (let ((pos)
+        (case-fold-search nil))
+    (while (and (setq pos (re-search-forward format-minor--shell-var-rx end t))
+                (null (nth 3 (syntax-ppss pos)))))
+    pos))
+
+
+(defvar format-minor--python-matchers
   '((format-minor--format-matcher (0 'format-minor-format-face prepend))
     (format-minor--printf-matcher (0 'format-minor-printf-face prepend)))
+  "Font-lock keyword matchers for `python-mode'.")
+
+
+(defvar format-minor--c/c++-matchers
+  '((format-minor--printf-matcher (0 'format-minor-printf-face prepend)))
+  "Font-lock keyword matchers for `c-mode' and `c++-mode'.")
+
+
+(defvar format-minor--rust-matchers
+  '((format-minor--format-matcher (0 'format-minor-format-face prepend)))
+  "Font-lock keyword matchers for `rust-mode'.")
+
+
+(defvar format-minor--shell-matchers
+  '((format-minor--shell-matcher
+     (1 'format-minor-format-face prepend)
+     (2 'format-minor-format-face prepend)))
+  "Font-lock keyword matchers for `sh-script-mode'.")
+
+
+(defvar format-minor--keywords-alist
+  `((python-mode    . ,format-minor--python-matchers)
+    (c-mode         . ,format-minor--c/c++-matchers)
+    (c++-mode       . ,format-minor--c/c++-matchers)
+    (rust-mode      . ,format-minor--rust-matchers)
+    (sh-script-mode . ,format-minor--shell-matchers))
   "Keywords given to `font-lock-add-keywords' and `font-lock-remove-keywords'.")
 
 
 (defun format-minor--enable ()
   "Enable additional format specifier highlighting."
-  (font-lock-add-keywords nil format-minor-font-lock-keywords))
+  (cl-loop for (mode . matchers) in format-minor--keywords-alist
+           do (font-lock-add-keywords mode matchers)))
 
 
 (defun format-minor--disable ()
   "Disable the additional format specifier highlighting."
-  (font-lock-remove-keywords nil format-minor-font-lock-keywords))
+  (cl-loop for (mode . matchers) in format-minor--keywords-alist
+           do (font-lock-remove-keywords mode matchers)))
 
 
 (define-minor-mode format-minor-mode
