0a394b13689fe3dd90a943673ef4e89b264af8fa
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2017-05-23 10:54:16 +0200

    Install: Added `terminfo` installation script.
    This script automatically installs an updated version of all terminal
    capabilities in `~/.terminfo`.

---
+++ scripts/install_terminfo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# Create a local `terminfo` database with more modern entries.
+tmp=$(mktemp --directory)
+url="http://invisible-island.net/datafiles/current/terminfo.src.gz"
+file=$(basename $url)
+wget --quiet $url --output-document=$tmp/$file
+gunzip -c $tmp/$file | tic -x -
+rm -rf $tmp
