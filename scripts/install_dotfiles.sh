66798ae28b4f91f2494e976f6b9d9ef7b3f96798
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2017-07-18 13:20:46 +0200

    Install: Only add supported SSH config.

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -8,6 +8,50 @@ mkdir --parents \
       $local_prefix_dir/share/man
 
 
+file_contains_line()
+{
+    line=${1:?"Missing string to search for."}
+    file=${2:?"Missing file to search in."}
+    grep -Fxq -e "${line}" -f ${file}
+}
+
+
+add_line_if_missing()
+{
+    line=${1:?"Missing string to search for."}
+    file=${2:?"Missing file to search in."}
+    output=${3:-${file}}
+    if ! file_contains_line "${line}" "${file}"; then
+        printf "${line}\n" >> ${output}
+    fi
+}
+
+
+add_supported_ssh_config()
+{
+    # Check for duplicates and unsupported options.
+    tmp=$(mktemp)
+    head=$(mktemp)
+    while IFS='' read -r line
+    do
+        add_line_if_missing "$line" "$HOME/.ssh/config" "$tmp"
+        case $(ssh -F $tmp _ 2>&1)
+        in
+            *"Bad configuration option:"*)
+                # Unsupported option remove the line.
+                head -n -1 $tmp > $head && cp $head $tmp
+                ;;
+            *"Name or service not known"*)
+                # Supported option.
+                ;;
+        esac
+    done < $dotfiles_dir/configs/ssh_config
+
+    cat $tmp >> $HOME/.ssh/config
+    rm -r $tmp $head
+}
+
+
 create_linkfarm()
 {
     # Create a recrusive linkfarm.
@@ -22,9 +66,10 @@ create_linkfarm()
     done
 }
 
+
 # Install common ssh configuration.
 mkdir --parents $HOME/.ssh
-cat $dotfiles_dir/configs/ssh_config >> $HOME/.ssh/config
+add_supported_ssh_config
 
 
 # Install all configuration files and plugins.
