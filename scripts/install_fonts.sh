495f7dcc21af08fc5f54339e9bb7e8900be4621e
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2017-09-11 14:20:29 +0200

    Install::fonts: "Fira Code Symbols".
    Minor tweaks to improve quoting, naming of temporary files, etc.

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -9,31 +9,37 @@ mkdir --parents \
       $fonts
 
 # Download and install Adobe Source Code Pro.
-url=https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
+url="https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip"
 wget $url --output-document=$tmp/source_code_pro.zip --quiet
 unzip -q $tmp/source_code_pro.zip -d $tmp
 find $tmp -name "*.otf" | xargs -I {} mv {} $fonts
 
 # Download and install Fira Code.
-url=https://github.com/tonsky/FiraCode/releases/download/1.204/FiraCode_1.204.zip
+url="https://github.com/tonsky/FiraCode/releases/download/1.204/FiraCode_1.204.zip"
 wget $url --output-document=$tmp/fira_code.zip --quiet
 unzip -q $tmp/fira_code.zip -d $tmp
 mv $tmp/ttf/*.ttf $fonts
 
+# Download the Fira Code Symbols font.
+url="https://github.com/tonsky/FiraCode/files/412440/FiraCode-Regular-Symbol.zip"
+wget $url --output-document=$tmp/fira_code_symbols.zip --quiet
+unzip -q $tmp/fira_code_symbols.zip -d $tmp
+mv $tmp/*.otf $fonts
+
 # Download and install Monoid.
 url="https://raw.githubusercontent.com/larsenwork/monoid/2db2d289f4e61010dd3f44e09918d9bb32fb96fd/Monoid.zip"
-wget $url --output-document=$tmp/fira_code.zip --quiet
-unzip -q $tmp/fira_code.zip -d $tmp
+wget $url --output-document=$tmp/monoid.zip --quiet
+unzip -q $tmp/monoid.zip -d $tmp
 mv $tmp/*.ttf $fonts
 
 # Download and install Iosevka.
-url=https://github.com/be5invis/Iosevka/releases/download/v1.12.3/iosevka-pack-1.12.3.zip
-wget $url --output-document=$tmp/fira_code.zip --quiet
-unzip -q $tmp/fira_code.zip -d $tmp
+url="https://github.com/be5invis/Iosevka/releases/download/v1.12.3/iosevka-pack-1.12.3.zip"
+wget $url --output-document=$tmp/iosevka.zip --quiet
+unzip -q $tmp/iosevka.zip -d $tmp
 mv $tmp/*.ttc $fonts
 
 # Download and install Font-awesome icons.
-url=http://fontawesome.io/assets/font-awesome-4.7.0.zip
+url="http://fontawesome.io/assets/font-awesome-4.7.0.zip"
 wget $url --output-document=$tmp/font-awesome-4.7.0.zip --quiet
 unzip -q $tmp/font-awesome-4.7.0.zip -d $tmp
 mv $tmp/font-awesome-4.7.0/fonts/*.woff $fonts
@@ -42,4 +48,4 @@ mv $tmp/font-awesome-4.7.0/fonts/*.woff $fonts
 rm -rf $tmp
 
 # Update the font cache.
-fc-cache
+fc-cache -f
