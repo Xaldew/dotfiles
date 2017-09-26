0bcda35e494605614b0472e50a22911a6401e6ee
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2017-09-26 16:40:44 +0200

    Emacs: Simplified Hasklig ligature code.

--- configs/emacs.d/elisp/hasklig-ligatures.el
+++ configs/emacs.d/elisp/hasklig-ligatures.el
@@ -13,32 +13,60 @@
 
 
 (defconst hasklig-ligatures
-  '("&&" "***" "*>" "\\\\" "||" "|>" "::"
-    "==" "===" "==>" "=>" "=<<" "!!" ">>"
-    ">>=" ">>>" ">>-" ">-" "->" "-<" "-<<"
-    "<*" "<*>" "<|" "<|>" "<$>" "<>" "<-"
-    "<<" "<<<" "<+>" ".." "..." "++" "+++"
-    "/=" ":::" ">=>" "->>" "<=>" "<=<" "<->")
-  "Strings that correspond to a ligature in the Hasklig font.")
+  '(("&&"   . #xe100)
+    ("***"  . #xe101)
+    ("*>"   . #xe102)
+    ("\\\\" . #xe103)
+    ("||"   . #xe104)
+    ("|>"   . #xe105)
+    ("::"   . #xe106)
+    ("=="   . #xe107)
+    ("==="  . #xe108)
+    ("==>"  . #xe109)
+    ("=>"   . #xe10a)
+    ("=<<"  . #xe10b)
+    ("!!"   . #xe10c)
+    (">>"   . #xe10d)
+    (">>="  . #xe10e)
+    (">>>"  . #xe10f)
+    (">>-"  . #xe110)
+    (">-"   . #xe111)
+    ("->"   . #xe112)
+    ("-<"   . #xe113)
+    ("-<<"  . #xe114)
+    ("<*"   . #xe115)
+    ("<*>"  . #xe116)
+    ("<|"   . #xe117)
+    ("<|>"  . #xe118)
+    ("<$>"  . #xe119)
+    ("<>"   . #xe11a)
+    ("<-"   . #xe11b)
+    ("<<"   . #xe11c)
+    ("<<<"  . #xe11d)
+    ("<+>"  . #xe11e)
+    (".."   . #xe11f)
+    ("..."  . #xe120)
+    ("++"   . #xe121)
+    ("+++"  . #xe122)
+    ("/="   . #xe123)
+    (":::"  . #xe124)
+    (">=>"  . #xe125)
+    ("->>"  . #xe126)
+    ("<=>"  . #xe127)
+    ("<=<"  . #xe128)
+    ("<->"  . #xe129))
+  "Mapping of regular expressions to a Unicode point in the Hasklig font.")
 
 
-(defconst hasklig-unicode-start #Xe100
-  "Start of the Unicode PUA used by the Hasklig font.")
-
-
-(defun hasklig-replacements (ligatures uc-beg)
-  "Create a list of replacement strings from LIGATURES.
-
-The Unicode code-points start from UC-BEG."
-  (let ((code-points (number-sequence uc-beg (+ uc-beg (length ligatures) -1))))
-    (cl-loop for (str . cp) in (cl-mapcar #'cons ligatures code-points)
-             collect (cons str (string ?\t cp)))))
+(defun hasklig-replacements (ligatures)
+  "Create a list of replacement strings from LIGATURES."
+  (cl-loop for (str . cp) in ligatures
+           collect (cons str (string ?\t cp))))
 
 
 (defun hasklig-setup-prettify-symbol ()
   "Add Hasklig ligatures for use with `mode/prettify-symbols-mode'."
-  (let ((replacements (hasklig-replacements hasklig-ligatures
-                                            hasklig-unicode-start)))
+  (let ((replacements (hasklig-replacements hasklig-ligatures)))
     (setq prettify-symbols-alist (append replacements prettify-symbols-alist))))
 
 
