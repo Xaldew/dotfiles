1bcd0f68d835d5b6f62e5c4ab15e6d5cfdbbe5d7
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2017-09-29 14:01:40 +0200

    Emacs: Properly configured `dumb-jump`.

--- configs/emacs.d/config/addons.el
+++ configs/emacs.d/config/addons.el
@@ -633,7 +633,25 @@ and style."
   (add-hook 'prog-mode-hook #'ggtags-mode))
 
 
-(use-package sx ; StackExchange client for Emacs
+(use-package ivy
+  :ensure t
+  :defer t)
+
+
+(use-package dumb-jump
+  :ensure t
+  :defer t
+  :after ivy
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config
+  (setq dumb-jump-selector 'ivy))
+
+
+(use-package sx                         ; StackExchange client for Emacs
   :defer t
   :ensure t)
 
