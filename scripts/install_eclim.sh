67645949761b488cec552199f68992ee69efe803
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2017-10-29 20:07:00 +0100

    Install: Eclipse now installed in XDG_DATA_HOME.

--- scripts/install_eclim.sh
+++ scripts/install_eclim.sh
@@ -3,13 +3,14 @@
 # Install Eclipse.
 url="https://eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/oxygen/1a/eclipse-java-oxygen-1a-linux-gtk-x86_64.tar.gz&r=1"
 wget --output-document=${objects_dir}/eclipse-java-oxygen-1a-linux-gtk-x86_64.tar.gz ${url}
-tar xvzf ${objects_dir}/eclipse-java-oxygen-1a-linux-gtk-x86_64.tar.gz -C ${objects_dir}/
+tar xvzf ${objects_dir}/eclipse-java-oxygen-1a-linux-gtk-x86_64.tar.gz -C ${XDG_DATA_HOME}/
+ln -s ${XDG_DATA_HOME}/eclipse/eclipse ${local_prefix_dir}/bin/eclipse
 
 # Install the `eclim` server and client components.
 url="https://github.com/ervandew/eclim/releases/download/2.7.0/eclim_2.7.0.jar"
 wget --output-document=${objects_dir}/eclim_2.7.0.jar ${url}
 java \
     -Dvim.skip=true \
-    -Declipse.home=${objects_dir}/eclipse \
+    -Declipse.home=${XDG_DATA_HOME}/eclipse \
     -jar ${objects_dir}/eclim_2.7.0.jar \
     install
