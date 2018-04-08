6055e61da1f8aa025b2481b6f2a01397037e5c06
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2018-04-08 23:15:13 +0200

    Emacs::helm-dash: OpenCL and OpenGL_4 docsets.

--- configs/emacs.d/config/addons.el
+++ configs/emacs.d/config/addons.el
@@ -1088,7 +1088,7 @@ When `ERC' exits the SSH process is killed from `erc-kill-server-hook'."
   (set-variable-in-hook rust-mode-hook    helm-dash-docsets '("Rust"))
   (set-variable-in-hook shell-script-hook helm-dash-docsets '("Bash"))
   (set-variable-in-hook c-mode-hook       helm-dash-docsets '("C" "OpenCL"))
-  (set-variable-in-hook c++-mode-hook     helm-dash-docsets '("C++" "C"))
+  (set-variable-in-hook c++-mode-hook     helm-dash-docsets '("C" "C++" "OpenCL" "OpenGL_4"))
   (set-variable-in-hook cmake-mode-hook   helm-dash-docsets '("CMake"))
   :config
   (define-key helm-map (kbd "C-h") #'backward-delete-char))
