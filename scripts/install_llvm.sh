bb50437afba310a492db21945f9c74f403d2274f
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2019-06-12 23:13:23 +0200

    Scripts: Use git to install llvm.

--- scripts/install_llvm.sh
+++ scripts/install_llvm.sh
@@ -1,45 +1,24 @@
 #!/usr/bin/env sh
-# A small script to checkout everything related to llvm and clang for a
-# given release.
+# A small script to checkout everything related to llvm.
 
 cd $objects_dir
-release=${1-trunk}
-
-if [ -d $objects_dir/llvm ]; then
-    cd $objects_dir/llvm
-    svn update
+if [ -d $objects_dir/llvm-project ]; then
+    cd $objects_dir/llvm-project
+    git pull
 else
-    url_base=http://llvm.org/svn/llvm-project/
-    if [ "$release" != "trunk" ]; then
-        release=tags/$release/final
-    fi
-    svn checkout $url_base/llvm/$release llvm
-    if [ $? -eq 0 ]; then
-	(cd llvm/tools &&
-             svn checkout $url_base/cfe/$release clang)
-        (cd llvm/tools/clang/tools &&
-             svn checkout $url_base/clang-tools-extra/$release extra)
-	(cd llvm/projects &&
-	     svn checkout $url_base/compiler-rt/$release compiler-rt)
-	(cd llvm/projects &&
-	     svn checkout $url_base/libcxx/$release libcxx)
-	(cd llvm/projects &&
-	     svn checkout $url_base/libcxxabi/$release libcxxabi)
-    else
-	echo "Error: Failed to download LLVM."
-    fi
+    git clone https://github.com/llvm/llvm-project.git
 fi
 
 # Compile llvm.
-cd $objects_dir/llvm
+cd $objects_dir/llvm-project
 mkdir -p build
 cd build
 cmake -G "Unix Makefiles" \
       -DCMAKE_C_COMPILER=${CC:-gcc} \
       -DCMAKE_CXX_COMPILER=${CXX:-g++} \
+      -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;lld;polly" \
       -DCMAKE_INSTALL_PREFIX=$local_prefix_dir \
-      -DLLVM_USE_SANITIZERS="${2}" \
       -DCMAKE_BUILD_TYPE=Release \
-      ..
+      ../llvm/
 make -j2
 make install
