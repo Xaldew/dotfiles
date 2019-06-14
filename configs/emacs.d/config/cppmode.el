788747ca2c0bd30ea86295c37f41af158f29edb8
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2019-06-14 18:30:41 +0200

    Emacs: Added llvm coding style.

--- configs/emacs.d/config/cppmode.el
+++ configs/emacs.d/config/cppmode.el
@@ -58,6 +58,32 @@
 
 ;;;; C++ coding style configuration.
 
+(defun llvm-lineup-statement (langelem)
+  "Line up arguments according to the LLVM style.
+
+LANGELEM is the element currently being inspected."
+  (let ((in-assign (c-lineup-assignments langelem)))
+    (if (not in-assign)
+        '++
+      (aset in-assign 0
+            (+ (aref in-assign 0)
+               (* 2 c-basic-offset)))
+      in-assign)))
+
+;; Add a cc-mode style for editing LLVM C and C++ code.
+(defconst my-llvm-style
+  '("gnu"
+    (fill-column . 80)
+    (c++-indent-level . 2)
+    (c-basic-offset . 2)
+    (indent-tabs-mode . nil)
+    (c-offsets-alist . ((arglist-intro . ++)
+                        (innamespace . 0)
+                        (member-init-intro . ++)
+                        (statement-cont . llvm-lineup-statement)))))
+(c-add-style "llvm.org" my-llvm-style)
+
+
 (defconst my-c++-style
   '("stroustrup"
     (indent-tabs-mode . nil)     ; Use spaces rather than tabs.
