72941b8e3847c4db76dfb5cd11547fa8939a9577
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2020-05-29 13:24:16 +0200

    Install: Ffmpeg URL updated.

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -3,12 +3,12 @@
 
 tmp=`mktemp --directory`
 cd $tmp
-wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
-tar xf ffmpeg-release-64bit-static.tar.xz --strip-components=1
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar xf ffmpeg-release-amd64-static.tar.xz --strip-components=1
 for f in *;
 do
     if [ -x $f -a -f $f ]; then
-       mv $f $local_prefix_dir/bin
+        mv $f $local_prefix_dir/bin
     fi
 done
 rm -r $tmp
