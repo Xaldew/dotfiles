450c3b307ce68faa24175b6b315c3a58183b4611
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2021-06-30 13:19:35 +0200

    Install: Plantuml file keeps the .jar extension.

--- scripts/install_plantuml.sh
+++ scripts/install_plantuml.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 url=http://sourceforge.net/projects/plantuml/files/plantuml.jar/download
-wget --quiet $url --output-document=$local_prefix_dir/bin/plantuml
-chmod u+x $local_prefix_dir/bin/plantuml
+wget --quiet $url --output-document=$local_prefix_dir/bin/plantuml.jar
+chmod u+x $local_prefix_dir/bin/plantuml.jar
