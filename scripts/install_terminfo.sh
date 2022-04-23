8733fd5d944a250ec41bc16be0472dad60ceb8d9
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2022-04-23 09:15:19 +0000

    More stable terminfo device handling.

--- scripts/install_terminfo.sh
+++ scripts/install_terminfo.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Create a local `terminfo` database with more modern entries.
+# Figure out where we have placed the script.
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 tmp=$(mktemp -d)
 url="http://invisible-island.net/datafiles/current/terminfo.src.gz"
 file=$(basename $url)
 wget --quiet $url --output-document=$tmp/$file
 gunzip -c $tmp/$file | tic -x -
 rm -rf $tmp
-tic -x $dotfiles_dir/configs/terminfo.src
+tic -x ${dir}/terminfo.src
