117047789777ef8ee77800bc6d86357bd4a785c9
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2022-06-14 16:18:57 +0200

    Scripts: Screen_gif.sh now uses xcb_rectsel.

--- scripts/screen_gif.sh
+++ scripts/screen_gif.sh
@@ -2,15 +2,20 @@
 # Record a selected screen region and output it as a GIF.
 
 out=${1:-out.gif}
-avi=$(mktemp XXXX_XXXX.avi)
-ffcast -s % ffmpeg -y -f x11grab -show_region 1 -framerate 15 \
-       -video_size %s -i %D+%c -codec:v huffyuv               \
-       -vf crop="iw-mod(iw\\,2):ih-mod(ih\\,2)" $avi
+fps=${2:-15}
+avi=${3-$(mktemp XXXX_XXXX.avi)}
 
-if [ $? -eq 0 ]; then
-    palette=$(mktemp XXXX_XXXX.png)
-    ffmpeg -y -loglevel error -i $avi -filter:v palettegen $palette
-    ffmpeg -y -loglevel error -i $palette -i $avi \
-           -filter_complex "[1:v][0:v] paletteuse" $out
-    rm $avi $palette
-fi
+xcb_rectsel | {
+    read xstart xend width height
+    ffmpeg -y -f x11grab -show_region 1 -framerate 15 \
+           -video_size ${width}x${height} -i ${DISPLAY:-":0.0"}+${xstart},${xend} -codec:v huffyuv               \
+           -vf crop="iw-mod(iw\\,2):ih-mod(ih\\,2)" $avi
+    clear
+    if [ $? -eq 0 ]; then
+        palette=$(mktemp XXXX_XXXX.png)
+        ffmpeg -y -loglevel error -i $avi -filter:v palettegen $palette
+        ffmpeg -y -loglevel error -i $palette -i $avi \
+               -filter_complex "[1:v][0:v] paletteuse" $out
+        rm $avi $palette
+    fi
+}
