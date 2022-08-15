cbdfbe96b073d1d67f23f3a981e6e958f59d7543
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2022-08-15 22:13:45 +0200

    Install: Script to install fx_cast_bridge.
    This is a service to automatically grab the URL from web-pages in
    Firefox and cast them to a Chromecast.

---
+++ scripts/install_fxcast.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# Install fx_cast and fx_cast_bridge.
+
+# Manually install the deb/rpm package from:
+# https://hensm.github.io/fx_cast/
+
+cat <<EOF | sudo tee /etc/systemd/system/fx_cast.service > /dev/null
+[Unit]
+Description=fx_cast daemon
+
+[Service]
+User=fx_cast
+ExecStart=/opt/fx_cast/fx_cast_bridge -d
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo useradd --system fx_cast
+sudo systemctl enable fx_cast.service
